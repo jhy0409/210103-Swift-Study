404d397b424e2cf07ec5fce75c575f7a16da1e67
jhy0409 <inooph@naver.com>
2021-04-26 08:55:39 +0900

    Revert "updated"
    This reverts commit b3bfee9acd05c389e0c5de102009f5ed81e8391f.

---
+++ 210209_FC_SwiftMine/210419_12_1to15_App_AppStMusic/chap12_10/AppleMusicStApp/AppleMusicStApp/AppleMusicStApp/Track/TrackManager.swift
@@ -0,0 +1,54 @@
+//
+//  TrackManager.swift
+//  AppleMusicStApp
+//
+//  Created by joonwon lee on 2020/01/12.
+//  Copyright © 2020 com.joonwon. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class TrackManager {
+    // TODO: 프로퍼티 정의하기 - 트랙들, 앨범들, 오늘의 곡
+    var tracks: [AVPlayerItem] = []
+    var albums: [Album] = []
+    var todaysTrack: AVPlayerItem?
+    
+    // TODO: 생성자 정의하기
+    init() {
+        let tracks = loadTracks()
+        self.tracks = tracks
+        self.albums = loadAlbums(tracks: tracks)
+        self.todaysTrack = self.tracks.randomElement()
+    }
+
+    // TODO: 트랙 로드하기
+    func loadAlbums(tracks: [AVPlayerItem]) -> [Album] {
+        let trackList = tracks.compactMap { $0.convertToTrack() }
+        let albumDics = Dictionary(grouping: trackList, by: { track in  track.albumName })
+        var albums: [Album] = []
+        for (key, value) in albumDics {
+            let title = key
+            let tracks = value
+            let album = Album(title: title, tracks: tracks)
+            albums.append(album)
+        }
+        return albums
+    }
+    
+    func loadTracks() -> [AVPlayerItem] {
+        let urls = Bundle.main.urls(forResourcesWithExtension: "mp3", subdirectory: nil) ?? []
+        let tracks = urls.map { AVPlayerItem(url: $0) }
+        return tracks
+    }
+    
+    func track(at index: Int) -> Track? {
+        return tracks[index].convertToTrack()
+    }
+
+    // TODO: 오늘의 트랙 랜덤으로 선책
+    func loadOtherTodaysTrack() {
+        self.todaysTrack = self.tracks.randomElement()
+    }
+}
