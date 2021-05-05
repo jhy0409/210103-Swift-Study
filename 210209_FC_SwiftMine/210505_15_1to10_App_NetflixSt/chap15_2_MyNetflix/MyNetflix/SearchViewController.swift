582745290644767bf702cda6013829def65401b1
jhy0409 <inooph@naver.com>
2021-05-05 20:07:59 +0900

    15-2강

--- 210209_FC_SwiftMine/210505_15_1to10_App_NetflixSt/chap15_2_MyNetflix/MyNetflix/SearchViewController.swift
+++ 210209_FC_SwiftMine/210505_15_1to10_App_NetflixSt/chap15_2_MyNetflix/MyNetflix/SearchViewController.swift
@@ -9,9 +9,37 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var resultCollectionView: UICollectionView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+}
+
+
+extension SearchViewController: UISearchBarDelegate {
+    
+    private func dismissKeyboard() {
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+
+        // 키보드 올라와 있을 때 내려가게 처리
+        dismissKeyboard()
+        // 검색어 있는지
+        
+        guard let searchTerm = searchBar.text, searchTerm.isEmpty == false else { return }
+        
+        // 네트워킹을 통한 검색
+        
+        
+        
+        
+        print("---> 검색어: \(searchTerm)")
+    }
 }
