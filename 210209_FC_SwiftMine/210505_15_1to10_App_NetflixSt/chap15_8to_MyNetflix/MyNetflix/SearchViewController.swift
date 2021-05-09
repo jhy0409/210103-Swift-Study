50d9993733430e8015e041703cf732e2e4066841
jhy0409 <inooph@naver.com>
2021-05-09 21:04:40 +0900

    15-9강

--- 210209_FC_SwiftMine/210505_15_1to10_App_NetflixSt/chap15_8to_MyNetflix/MyNetflix/SearchViewController.swift
+++ 210209_FC_SwiftMine/210505_15_1to10_App_NetflixSt/chap15_8to_MyNetflix/MyNetflix/SearchViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Kingfisher
+import AVFoundation
 
 class SearchViewController: UIViewController {
     
@@ -54,10 +55,14 @@ extension SearchViewController: UICollectionViewDelegate {
         // presenting player vc
         
         let movie = movies[indexPath.item]
+        let url = URL(string: movie.previewURL)!
+        let item = AVPlayerItem(url: url)
         
         let sb = UIStoryboard(name: "Player", bundle: nil)
         let vc = sb.instantiateViewController(identifier: "PlayerViewController") as! PlayerViewController
         vc.modalPresentationStyle = .fullScreen
+        
+        vc.player.replaceCurrentItem(with: item)
         present(vc, animated: false, completion: nil)
     }
 }
