20053e8fefddbe2a6770861a57748d9bbd395f42
jhy0409 <inooph@naver.com>
2021-05-09 20:47:13 +0900

    15-8강

---
+++ 210209_FC_SwiftMine/210505_15_1to10_App_NetflixSt/chap15_8to_MyNetflix/MyNetflix/PlayerViewController.swift
@@ -0,0 +1,43 @@
+//
+//  PlayerViewController.swift
+//  MyNetflix
+//
+//  Created by joonwon lee on 2020/04/01.
+//  Copyright © 2020 com.joonwon. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PlayerViewController: UIViewController {
+    
+    @IBOutlet weak var playerView: PlayerView!
+    @IBOutlet weak var controlView: UIView!
+    @IBOutlet weak var playButton: UIButton!
+    
+    let player = AVPlayer()
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .landscapeRight
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    @IBAction func togglePlaybutton(_ sender: Any) {
+        playButton.isSelected = !playButton.isSelected
+        
+    }
+    @IBAction func closeButtonTapped(_ sender: Any) {
+        dismiss(animated: false, completion: nil)
+    }
+}
+
+extension AVPlayer {
+    var isPlaying: Bool {
+        guard self.currentItem != nil else { return false }
+        return self.rate != 0
+    }
+}
