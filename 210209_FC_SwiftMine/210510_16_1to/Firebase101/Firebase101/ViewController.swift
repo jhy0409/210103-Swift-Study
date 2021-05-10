3c58e89a3aa2e502ee175f716e73ecfacc6a7543
jhy0409 <inooph@naver.com>
2021-05-10 16:13:53 +0900

    16-6강

--- 210209_FC_SwiftMine/210510_16_1to/Firebase101/Firebase101/ViewController.swift
+++ 210209_FC_SwiftMine/210510_16_1to/Firebase101/Firebase101/ViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
-
+import Firebase
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var dataLable: UILabel!
+    let db = Database.database().reference()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateLable()
     }
-
-
+    
+    func updateLable() {
+        db.child("firstData").observeSingleEvent(of: .value) { snapshot in
+            print("---> \(snapshot)")
+            
+            let value = snapshot.value as? String ?? ""
+            DispatchQueue.main.async {
+                self.dataLable.text = value
+            }
+        }
+    }
 }
 
