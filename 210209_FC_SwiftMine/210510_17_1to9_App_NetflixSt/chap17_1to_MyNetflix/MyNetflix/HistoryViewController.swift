4e5bac0d2ec9e6b0dffc1fea72337eca1f6d20e3
jhy0409 <inooph@naver.com>
2021-05-12 11:40:43 +0900

    17강 끝 (9of9)

--- 210209_FC_SwiftMine/210510_17_1to9_App_NetflixSt/chap17_1to_MyNetflix/MyNetflix/HistoryViewController.swift
+++ 210209_FC_SwiftMine/210510_17_1to9_App_NetflixSt/chap17_1to_MyNetflix/MyNetflix/HistoryViewController.swift
@@ -14,6 +14,7 @@ class HistoryViewController: UIViewController {
     @IBOutlet weak var TableView: UITableView!
     
     let db = Database.database().reference().child("searchHistory")
+    var searchTerms: [SearchTerm] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,23 +26,46 @@ class HistoryViewController: UIViewController {
         super.viewWillAppear(animated)
         
         db.observeSingleEvent(of: .value) { (snapshot) in
-            print("---> snapshot: \(snapshot.value)")
+            
+            guard let searchHistory = snapshot.value as? [String: Any] else { return }
+//            [String: [String: Any]
+            let data = try! JSONSerialization.data(withJSONObject: Array(searchHistory.values), options: [])
+            
+            let decoder = JSONDecoder()
+            let searchTerms = try! decoder.decode([SearchTerm].self, from: data)
+            self.searchTerms = searchTerms.sorted(by: { (term1, term2) in
+                return term1.timestamp > term2.timestamp
+            })
+            
+            // s s s s s
+            self.TableView.reloadData()
+            print("---> snapshot: \(data)+ \(searchTerms)++")
         }
     }
+}
+
+extension HistoryViewController: UITableViewDataSource {
+    // 몇개, 어떻게 보여줄지
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchTerms.count
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "HistoryCell", for: indexPath) as? HistoryCell else {
+            return UITableViewCell()
+        }
+        cell.searchTerm.text = searchTerms[indexPath.row].term
+        return cell
+    }
 }
 
 class HistoryCell: UITableViewCell {
     @IBOutlet weak var searchTerm: UILabel!
+}
+
+
+struct SearchTerm: Codable {
+    let term: String
+    let timestamp: TimeInterval
 }
