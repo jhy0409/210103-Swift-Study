ead6baa6795d212e8b84b4934cd7423ef66a2c50
jhy0409 <inooph@naver.com>
2021-05-16 20:00:09 +0900

    66강 Strings and Characters / 문자열과 문자

--- Mastering-Swift-master/13-Strings And Characters/1-Strings.playground/Contents.swift
+++ Mastering-Swift-master/13-Strings And Characters/1-Strings.playground/Contents.swift
@@ -25,73 +25,59 @@ import UIKit
  # Strings and Characters
  */
 
+let s = "String"
+let c: Character = "C"
 
+let emptyChar: Character = " "
+var emptyString = " " // 공백 포함한 문자열, print : 1
+emptyString.count
 
+emptyString = ""
+emptyString.count // 빈문자열, print : 0
 
-
-
-
-
-
-
-
+// 문자열 생성자로 빈문자 생성
+let emptyString2 = String()
 
 /*:
  ## String Types
  */
+/*
+ 1. String : 구조체로 구현돼있음, 값형식 (swift)
+ 2. NSString : 문자열 참조형식 처리 (Foundationg String)
+ 
+ 1 <-> 2 서로 호환
+ */
 
+var nsstr: NSString = "str"
+let swiftStr: String = nsstr as String
 
-
-
-
-
-
-
-
-
-
+nsstr = swiftStr as NSString
+// Toll-Free Bridged (타입캐스팅으로 호환되는 자료형)
 
 /*:
  ## Mutability
  */
+// 문자열의 가변성
+let immutableStr = "str"
+//immutableStr = "new str"
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+var mutableStr = "str2"
+mutableStr = "new str"
 
 /*:
  ## Unicode
  */
 
+let str = "Swift String"
+str.utf8
+str.utf16
 
+// 특정 인코딩 해야할 때 봐야할 문서 : string reference
 
+var thumbUp = "👍"
+thumbUp = "\u{1F44D}" // 유니코드 스칼라
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//👍
+//올린 엄지
+//유니코드: U+1F44D, UTF-8: F0 9F 91 8D
 
