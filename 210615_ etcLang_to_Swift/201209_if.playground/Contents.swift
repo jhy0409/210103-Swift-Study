338d40cb9957ffc805165726d898555faf3e3b78
jhy0409 <inooph@naver.com>
2021-06-15 08:43:44 +0900

    자바수업 코드 -> swift로 변환 (if, if else문)

---
+++ 210615_ etcLang_to_Swift/201209_if.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+/*
+ 20년 12월 9일 자바수업 if문
+ 
+ (원본링크)
+ https://github.com/jhy0409/jhy0409/tree/main/3%20JavaClass/201209_if/src
+ 
+ public class IF1 {
+     public static void main(String[] args) {
+         int num = 99;
+         
+         if (num < 100) {
+             System.out.println("num가 100보다 작다.");
+         }
+     }
+ }
+ */
+
+var num : Int = 99
+if num < 100 {
+    print("num(\(num))은 100보다 작다.")
+}
+
