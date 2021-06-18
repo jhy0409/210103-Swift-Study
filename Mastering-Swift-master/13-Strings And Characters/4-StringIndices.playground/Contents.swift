9e0f8ad7c4837b53382e00d0091a8c658df85058
jhy0409 <inooph@naver.com>
2021-06-18 13:15:37 +0900

    인프런 71강 String Indices

--- Mastering-Swift-master/13-Strings And Characters/4-StringIndices.playground/Contents.swift
+++ Mastering-Swift-master/13-Strings And Characters/4-StringIndices.playground/Contents.swift
@@ -24,21 +24,32 @@ import UIKit
 /*:
  # String Indices
  */
+// 문자열 인덱스
+let str = "Swift"
+let firstCh = str[str.startIndex]
+print(firstCh)
 
+let lastCharIndex = str.index(before: str.endIndex)
+let lastCh = str[lastCharIndex]
+print(lastCh)
 
+let secondCharIndex = str.index(after: str.startIndex)
+let secondCh = str[secondCharIndex]
+print(secondCh)
 
+// 처음, 끝 인덱스 저장안했을 때 사용
+var thirdCharIndex = str.index(str.startIndex, offsetBy: 2)
+var thirdCh = str[thirdCharIndex]
+print(thirdCh)
 
+// end인덱스는 마지막 인덱스 다음 인덱스,
+// 마지막 인덱스 기준으로 사용할 때는 -N으로 입력해야함
+// 잘못된 인덱스 사용 : 앱에서 크래시 발생, 확신 어려울 시 if문
+thirdCharIndex = str.index(str.endIndex, offsetBy: -3)
+thirdCh = str[thirdCharIndex]
+print(thirdCh)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+// 유니코드 독립적 형태로 사용하기 때문에 정수사용 불가능
+if thirdCharIndex < str.endIndex && thirdCharIndex >= str.startIndex {
+    
+}
