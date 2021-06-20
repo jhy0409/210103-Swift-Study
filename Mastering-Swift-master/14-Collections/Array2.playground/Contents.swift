f6c00eddccb86d887951e53fd260ccc18a4d2072
jhy0409 <inooph@naver.com>
2021-06-20 14:41:04 +0900

    Swift 83강 - 배열에 새로운 요소를 추가하고 삭제

--- Mastering-Swift-master/14-Collections/Array2.playground/Contents.swift
+++ Mastering-Swift-master/14-Collections/Array2.playground/Contents.swift
@@ -24,33 +24,62 @@ import UIKit
 /*:
  # Adding Elements
  */
+// 배열 편집
+var alphabet = ["A", "B", "C"]
+alphabet.append("E") // 마지막에 동일자료형 추가
 
+alphabet.append(contentsOf: ["F", "G"])
+alphabet.insert("D", at: 3)
 
+alphabet.insert(contentsOf: ["a", "b", "c"], at: 0)
+// 인서트는 인덱스 이동하므로 오버헤드 발생, 꼭 필요한 경우에만 사용 주로 append
 
+// 배열 일부분 바꾸기
+// 1- subscript 문법
+alphabet[0...2] = ["x", "y", "z"]
+alphabet
 
+// 2- 메소드로 바꾸기
+alphabet.replaceSubrange(0...2, with: ["a", "b", "c"])
+alphabet
 
+// 부분 교체
+alphabet[0...2] = ["z"]
+alphabet
 
-
+// 부분 삭제
+alphabet[0..<1] = []
+alphabet
 
 /*:
  # Removing Elements
  */
+// 요소 삭제
 
+alphabet = ["A", "B", "C", "D", "E", "F", "G"]
+alphabet.remove(at: 2)
+alphabet
 
+alphabet.removeFirst()
+alphabet
 
+alphabet.removeFirst(2)
+alphabet
 
+alphabet.removeLast()
+alphabet
 
+alphabet.removeAll() // 인덱스, 배열이 비어있는지 확인 필요
+alphabet.popLast() // 좀 더 안전한 코드
 
+alphabet = ["A", "B", "C", "D", "E", "F", "G"]
+alphabet.popLast()
+alphabet
 
+alphabet.removeSubrange(0...2)
+alphabet
 
-
-
-
-
-
-
-
-
-
+alphabet[0...2] = []
+alphabet
 
 
