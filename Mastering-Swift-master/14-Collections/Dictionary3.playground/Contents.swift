edd5642c72dee666a1e8759ad10ceb8d9dac59a8
jhy0409 <inooph@naver.com>
2021-06-20 17:13:36 +0900

    Swift 87강 - Dictionary #3 / 딕셔너리 비교하고 요소 검색

--- Mastering-Swift-master/14-Collections/Dictionary3.playground/Contents.swift
+++ Mastering-Swift-master/14-Collections/Dictionary3.playground/Contents.swift
@@ -20,41 +20,41 @@
 //  THE SOFTWARE.
 //
 import UIKit
-
-
 /*:
  # Comparing Dictionaries
  */
-
 let a = ["A": "Apple", "B": "Banana", "C": "City"]
-let b = ["A": "Apple", "C": "City", "B": "Banana"]
+let b = ["A": "Apple", "C": "City", "B": "banana"]
+// 정렬되어있지 않기때문에 키와 값이 같으면 같은 딕셔너리로 판별
+a == b
+a != b
+//a.elementsEqual(b) { (lhs, rhs) -> Bool in
+//    print(lhs.key, rhs.key)
+//    return lhs.key.caseInsensitiveCompare(rhs.key) == .orderedSame && lhs.value.caseInsensitiveCompare(rhs.value) == .orderedSame
+//}
+let aKeys = a.keys.sorted()
+let bkeys = b.keys.sorted()
 
-
-
-
-
-
-
-
+aKeys.elementsEqual(bkeys) { (lhs, rhs) -> Bool in
+    guard lhs.caseInsensitiveCompare(rhs) == .orderedSame else {
+        return false
+    }
+    
+    guard  let lv = a[lhs], let rv = b[rhs], lv.caseInsensitiveCompare(rv) == .orderedSame else {  return false
+    }
+    print(lhs, rhs); print(lv, rv); return true
+}
 /*:
  # Finding Elements
  */
+var words = ["A": "Apple", "B": "Banana", "C": "City"]
+let c: ((String, String)) -> Bool = {
+    $0.0 == "B" || $0.1.contains("i")
+}
 
+words.contains(where: c)
+let r = words.first(where: c) // 옵셔널 튜플
+r?.key
+r?.value
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+words.filter(c)
