d256d074d97f0a761ced9864803d1b8d438ed873
jhy0409 <inooph@naver.com>
2021-06-21 14:23:07 +0900

    Swift 97강 - Value Types vs Reference Types / 값 형식과 참조 형식의 차이점

--- Mastering-Swift-master/16-Structures And Classes/Value-vs-Reference.playground/Contents.swift
+++ Mastering-Swift-master/16-Structures And Classes/Value-vs-Reference.playground/Contents.swift
@@ -25,4 +25,67 @@ import UIKit
  # Value Types and Reference Types
  ![val-ref](val-ref.png)
  */
+/*
+ 값 형식, 참조형식 차이
+ 
+ 1. 값형식
+ - 스트럭쳐
+ - 열거형
+ - 튜플
+ - 컬렉션
+ 
+ 2. 참조형식
+ - 클래스
+ - 클로저
+ */
 
+struct PositionValue { // 선언과 동시에 기본값 저장
+    var x = 0.0
+    var y = 0.0
+}
+
+class PositionObject {
+    var x = 0.0
+    var y = 0.0
+}
+
+var v = PositionValue()
+var o = PositionObject()
+v.x
+v.y
+o.x
+o.y
+
+var v2 = v // 같은 값 가진 복사본
+var o2 = o
+v2.x
+v2.y
+o2.x
+o2.y
+
+v2.x = 12
+v2.y = 34
+
+v.x
+v.y
+v2.x
+v2.y
+
+o2.x = 12
+o2.y = 34
+
+// 참조형식 : 복사본 생성하지 않고 인스턴스 저장된 메모리 주소 전달
+o.x
+o.y
+
+o2.x
+o2.y
+/*
+ 1. 값형식 사용
+ - 적은데이터 저장, 상속이 필요없을 때
+ - 값 전달시점마다 복사본 필요
+ 
+ 2. 열거형 : 연관된 상수그룹
+ 3. 튜플  : 코드 내 한번만 사용(나머지 다 구조체로)
+ 4. 클래스 : 상속, 참조 전달해야하는 경우
+ */
