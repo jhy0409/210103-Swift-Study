5308044c066f17293a1ae89eff5d350e79d76bd9
jhy0409 <inooph@naver.com>
2021-06-21 10:16:52 +0900

    Swift 93강 - Associated Values / case에 연관 값 저장

--- Mastering-Swift-master/15-Enumerations/AssociatedValues.playground/Contents.swift
+++ Mastering-Swift-master/15-Enumerations/AssociatedValues.playground/Contents.swift
@@ -25,22 +25,33 @@ import UIKit
  # Associated Values
  ![associated-values](associated-values.png)
  */
+//enum VideoInterface: String {
+//    case dvi = "1028X768"
+//    case hdmi = "2048X1536"
+//    case displayPort = "3840X2160"
+//}
 
+// 연관값 : 새로운 열거형값 생성할 때 사용
 
+enum VideoInterface {
+    case dvi(width: Int, height: Int)
+    case hdmi(Int, Int, Double, Bool)
+    case displayPort(CGSize)
+}
 
+var input = VideoInterface.dvi(width: 2048, height: 1536)
 
+switch input {
+case .dvi(2048, 1536):
+    print("dvi 2048 X 1536")
+case .dvi(2048, _): // 너비만 매칭
+print("dvi 2048 X Any")
+case .dvi: // 연관값 무시 케이스만 매칭
+    print("dvi")
+case .hdmi(let width, let height, let version, var audioEnabled):
+    print("hdml \(width) x \(height)")
+case let .displayPort(size):
+    print("dp") // 모든 연관값 동일형태로 바인딩(=let)
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+input = .hdmi(3840, 2160, 2.1, true)
