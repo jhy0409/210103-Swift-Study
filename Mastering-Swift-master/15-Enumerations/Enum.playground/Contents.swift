7ace7212d7a7a7fc5e7caa0741b567e45a6fde8a
jhy0409 <inooph@naver.com>
2021-06-21 09:41:47 +0900

    Swift 91강 - Enumeration Types / 열거형 선언, 사용

--- Mastering-Swift-master/15-Enumerations/Enum.playground/Contents.swift
+++ Mastering-Swift-master/15-Enumerations/Enum.playground/Contents.swift
@@ -25,38 +25,59 @@ import UIKit
  # Enumerations
  ![enum](enum.png)
  */
+/*
+상수 -> Enumeration Case(or Case)
+독립적 자료형
+
+ 사용이유
+ 1. 가독성
+ 2. 안정성
+ */
+
+let left = 0
+let center = 1
+let right = 2
 
 
+//let left = "left"
+//let center = "center"
+//let right = "right"
 
+/*
+ if alignment = "Center" { // 대소문자 구분으로 실행안됨
+ 
+ }
+ */
 
-
-
-
-
-
+var alignment = center
 
 /*:
  ## Syntax
  ![syntax](enum-syntax.png)
  */
+enum Alignment {
+    case left
+    case right
+    case center
+}
 
+Alignment.center // 독립적인 값
+var textAlignment = Alignment.center // 열거형으로 저장했기 때문에
+textAlignment = .left // .케이스 이름만 써도 에러 안남
 
+//textAlignment = .justify // 저장되지 않은 값 저장 불가능
+//textAlignment = .Left // 오타 허용X
 
+if textAlignment == .center {
+    
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+switch textAlignment {
+case .left:
+    print("left")
+case .right:
+    print("right")
+case .center:
+    print("center")
+}
 
