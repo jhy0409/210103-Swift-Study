45892f52ffe0cd293f790d5c10c10bb6674a750f
jhy0409 <inooph@naver.com>
2021-06-22 16:24:33 +0900

    Swift 107강 - Subscripts / [스퀘어브라켓 : 덱스, 키, 범위]

--- Mastering-Swift-master/18-Methods And Subscripts/Subscripts.playground/Pages/Syntax.xcplaygroundpage/Contents.swift
+++ Mastering-Swift-master/18-Methods And Subscripts/Subscripts.playground/Pages/Syntax.xcplaygroundpage/Contents.swift
@@ -26,27 +26,82 @@ import UIKit
  # Subscripts
  ![subscript](subscript.png)
  */
+/*
+ 서브스크립트 : [스퀘어브라켓 : 덱스, 키, 범위]
+ */
 
-
-
-
-
-
-
-
-
-
+let list = ["A", "B", "C"]
+list[0]
 
 /*:
  ![subscript-syntax](subscript-syntax.png)
  */
+/*
+ 서브스크립트 구현
+ 
+ [불가능]
+ - 입출력 파라미터나 파라미터 기본값 지정 X
+ - 서브스크립트로 값 받아야하므로 파라미터 생략 X
+ - 리턴형 생략 불가능 (리턴값, 저장값이기 때문)
+ 
+ 생략 : 서브스크립트에서 값 저장 안할 때
+ 최소 get block은 있어야 함
+ 
+ */
 
+class List {
+    var data = [1, 2, 3]
+    
+    subscript(i index: Int) -> Int {
+        get {
+            return data[index]
+        }
+        
+        set {
+            data[index] = newValue
+        }
+    }
+}
 
+var l = List()
+l[i: 0]
+l[i: 1] = 123
+l.data
 
+/*
+ argument lable : 두개이상의 파라미터 선언, 파라미터 가독성 높일 때 제한적 사용
+ */
 
+// 구조체에서 서브스크립트 구현
+struct Matrix {
+    var data = [[1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9]]
+    
+    subscript(row: Int, col: Int) -> Int {
+        return data[row][col]
+    } // 읽기전용 서브스크립트, get {}생략 됨
+}
 
+let m = Matrix()
+m[0,0]
+// m[0,10]
 
-
-
-
+// ---- index 유효값 검사, if & optional
+/*
+ struct Matrix2 {
+ var data = [[1, 2, 3],
+ [4, 5, 6],
+ [7, 8, 9]]
+ 
+ subscript(row: Int, col: Int) -> Int? {
+ if row < data.count && col < data[0].count { // 행의개수, 열의개수 미만 3이면 0~2까지
+ return data[row][col]
+ } else { return nil }
+ } // 읽기전용 서브스크립트, get {}생략 됨
+ }
+ 
+ let test = Matrix()
+ m[0,4]
+ */
 
