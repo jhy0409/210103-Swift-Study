16144d871a5b23f6b8faf1f47205f1ec2e53fb7f
jhy0409 <inooph@naver.com>
2021-06-22 15:14:21 +0900

    Swift 105강 - Instance Method / 인스턴스와 연관된 동작 구현

--- Mastering-Swift-master/18-Methods And Subscripts/IntanceMethods.playground/Pages/Syntax.xcplaygroundpage/Contents.swift
+++ Mastering-Swift-master/18-Methods And Subscripts/IntanceMethods.playground/Pages/Syntax.xcplaygroundpage/Contents.swift
@@ -26,30 +26,64 @@ import UIKit
  ![Method](method.png)
  ![Call Method](call-method.png)
  */
+/*
+ 인스턴스 메소드 : 클래스, 구조체, 열거형에서 구현
+ 인스턴스와 연관된 동작 구현
+ */
 
+class Sample {
+    var data = 0
+    static var sharedData = 123
+    
+    func doSomeThing() {
+        print("\(#function)")
+        print("data : \(data)")
+        Sample.sharedData // 인스턴스 메소드에서 타입멤버의 접근
+    }
+    
+    func call() {
+        print("\n\(#function)")
+        doSomeThing()
+    }
+}
 
+let a = Sample()
+a.data
+a.doSomeThing()
+a.call()
 
+class Size1 {
+    var width = 0.0
+    var height = 0.0
+    
+    func enlarge() {
+        width += 1.0
+        height += 1.0
+    }
+}
 
+let s_class = Size1()
+s_class.width
+s_class.height
 
+s_class.enlarge()
+s_class.width
+s_class.height
 
+struct Size2 {
+    var width = 0.0
+    var height = 0.0
+    
+    mutating func enlarge() { // mutating : 구조체에서 속성변경 위함
+        width += 1.0
+        height += 1.0
+    }
+}
 
+var s_struct = Size2() // 값형식 : 인스턴스의 가변성 -> 멤버의 가변성에 영향 줌
+s_struct.width
+s_struct.height
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+s_struct.enlarge()
+s_struct.width
+s_struct.height
