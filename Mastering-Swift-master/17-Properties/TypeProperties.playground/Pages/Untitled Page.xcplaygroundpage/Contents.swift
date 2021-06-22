7d31fe07c6fcbe9dc993f3a5dfbb822454f855ae
jhy0409 <inooph@naver.com>
2021-06-22 11:37:56 +0900

    Swift 103강 - Type Property / 형식속성

--- Mastering-Swift-master/17-Properties/TypeProperties.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ Mastering-Swift-master/17-Properties/TypeProperties.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -28,11 +28,23 @@ import UIKit
  ![type1](type1.png)
  ![type2](type2.png)
  */
+/*
+ 형식속성(Type Property) :
+    - 특징 : 타입의 속성, 모든 인스턴스가 공유하는 하나의 공간만 생성
+    - 기본값 : 기본값생략 불가능, (->생성자로 생성되지 않아서)
+    - 추가 : 클래스, 구조체, 열거형 모두 가능
+    - 접근 : 형식이름.속성이름
+ 
+ 1. 저장형식 속성 : 속성 최초 접근시 초기화 (형식속성 = 지연속성)
+ 형식과 연관된 메소드를 선언, 접근
+ */
 
+class Math {
+    static let pi = 3.14
+}
 
-
-
-
+let m = Math()
+Math.pi // 저장 형식속성(pi속성) -> 접근시점에서 초기화 됨
 
 
 /*:
@@ -40,14 +52,21 @@ import UIKit
  ![type3](type3.png)
  ![type4](type4.png)
  */
+/*
+ static 선언      : 서브클래스에서 오버라이딩 허용
+ class 키워드 선언  : 오버라이딩 허용
+ */
 
+enum Weekday: Int {
+    case sun = 1, mon, tue, wed, thur, fri, sat
+    
+    static var today: Weekday {
+        let cal = Calendar.current
+        let today = Date() // 오늘날짜
+        let weekday = cal.component(.weekday, from: today) // 요일 나타내는 정수 리턴
+        return Weekday(rawValue: weekday)!
+    }
+}
 
-
-
-
-
-
-
-
-
+Weekday.today
 
