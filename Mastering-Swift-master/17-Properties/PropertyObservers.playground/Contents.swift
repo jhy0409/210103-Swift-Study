66ae68cb2599dd509a3ca9d35ec4baecae08edd4
jhy0409 <inooph@naver.com>
2021-06-22 10:55:19 +0900

    Swift 102강 - Property Observer / 속성 감시자

--- Mastering-Swift-master/17-Properties/PropertyObservers.playground/Contents.swift
+++ Mastering-Swift-master/17-Properties/PropertyObservers.playground/Contents.swift
@@ -25,8 +25,36 @@ import UIKit
  # Property Observer
  ![property-observer](property-observer.png)
  */
-
+/*
+ 속성감시자
+ willset : 속성에 값 저장되기 직전 호출 (매개변수: 새로저장되는 값 기본(newValue))
+ didset  : 값 저장된 직후, 이전값이 파라미터에 저장됨 (기본(oldValue))
+ 
+ 변수 저장속성에 추가 가능
+ 
+ 
+ [ 추가 불가능 ]
+ - 상수    : X
+ - 지연    : (구현조건) 초기화 코드에서 구현
+ - 계산속성 : (구현조건) set블록에서 구현
+ 
+ - 예외    :
+    - 서브클래스 계산속성 오버라이딩, 속성감시자 구현 가능
+    - 기능수행 위해서는 willSet, didSet 둘 중 하나는 반드시 구현 해야함
+ 
+ */
 class Size {
-   var width = 0.0
+    var width = 0.0 {
+        willSet {
+            print("willSet\t> 이전 : \(width) => 변경 :\(newValue)")
+        }
+        didSet {
+            print("didSet\t> 이전 : \(oldValue) => 변경 :\(width)")
+        }
+    }
 }
 
+let s = Size()
+s.width = 123
+print()
+s.width = 55
