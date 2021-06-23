4cef461a8114b1e2e4041e94403f0ce3c52934a3
jhy0409 <inooph@naver.com>
2021-06-23 13:15:19 +0900

    Swift 111강 - Upcasting and Downcasting

--- Mastering-Swift-master/19-Inheritance/UpcastingDowncasting.playground/Contents.swift
+++ Mastering-Swift-master/19-Inheritance/UpcastingDowncasting.playground/Contents.swift
@@ -48,13 +48,41 @@ class Rectangle: Figure {
 }
 
 class Square: Rectangle {
-   
 }
 
+let f = Figure(name: "Unknown")
+f.name
 
+let r = Rectangle(name: "Rect")
+r.width
+r.height
+r.name
 
+// Upcasting : 서브클래스 인스턴스를 슈퍼클래스 형식으로 저장
+// - 동일한 클래스에서 업캐스팅 : 안전(에러 미발생)해서 별도 문법 필요 X
+let s: Figure = Square(name: "Square") // 1 - 3
+s.name
+/*
+ -- S(F(이름), 너비, 높이) -> F로 선언 -> 이름만 접근가능
+ s.width
+ s.height
+ */
 
+// 1 Figure > 2 Rectangle > 3 Sqaure > 4 Rhombus
+let downcasted = s as! Square // 업캐스팅 된 클래스의 서브 클래스, 원본클래스의 슈퍼클래스 다운캐스팅 가능
+downcasted.name
+downcasted.width
+downcasted.height
 
+/*
+ - 다운캐스팅 : upcasting된걸 내리는 거
+     - 에러 발생할 수 ㅇ
+     - 항상 성공하지는 않음
+ */
 
+class Rhombus: Square {
+    var angle = 45.0
+}
 
-
+// let dr = s as! Rhombus // angle 속성이 없으므로 실패
+// 원본보다 아래인 서브클래스로 다운캐스팅 허용되지 않음
