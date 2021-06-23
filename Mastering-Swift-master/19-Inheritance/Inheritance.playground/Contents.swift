715e0d8a9f244cf0bc98af4ac3396a19a3287110
jhy0409 <inooph@naver.com>
2021-06-23 11:16:34 +0900

    Swift 109강 - Inheritance / 클래스 상속

--- Mastering-Swift-master/19-Inheritance/Inheritance.playground/Contents.swift
+++ Mastering-Swift-master/19-Inheritance/Inheritance.playground/Contents.swift
@@ -25,20 +25,75 @@ import UIKit
  # Inheritance
  ![inheritance](inheritance.png)
  */
+/*
+ 상속
+ 
+ 교통수단
+ 공통특징
+ 
+ 탑승인원 속성
+ 1. 네개의 클래스, 개별선언 -> 코드중복
+ 문제 : 따로 다 고쳐야함 > 속성이름이나 형식(Type or etc) 변경
+ 
+ 2. 상속클래스
+ - 모든 클래스 상속받는 클래스 선언
+ - 속성 추가
+ 
+ 상속관계의 클래스   : 클래스 계층을 구성
+ 최상위           : Root, Base, Parent, Super class
+ 상속받는 하위 클래스 : Child Class, Sub Class
+ 
+ --- * 베이스 -> 하위 서브클래스 존재하지만 슈퍼클래스는 존재하지 않음
+ Objective-C : 모든클래스 상속해야 함 (NSObject)
+ Swift       : 제약이 없음
+ 
+ [가능]
+ - 여러 서브 클래스가 공통 super class 상속
+ 
+ [불가능]
+ - 두개이상의 슈퍼클래스 상속(다중상속 미지원) -> 프로토콜 활용하여 지원
+ 
+ * Subclassing   : 다른클래스 상속
+ - 오버라이딩(재정의) : 구현수정
+ */
 
 
+// Base Class - 도형 추상화 클래스
+class Figure {
+    var name = "Unknown"
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func draw() {
+        print("draw \(name)")
+    }
+}
 
+class Circle: Figure {
+    var radius = 0.0
+}
 
-
-
+let c = Circle(name: "Circle") // 부모클래스의 생성자 및 멤버 상속받음
+c.radius
+c.name // 부모클래스
+c.draw() // 부모클래스
 
 /*:
  ## final class
  ![final class](final-class.png)
  */
+// 상속 금지
+final class Rectangle: Figure { // 사각형 < 도형, 다른클래스에서 상속받는건 가능
+    var width = 0.0
+    var height = 0.0
+}
 
-
-
+// 정사각형 < 사각형 < 도형
+// -------------------> final클래스에서 상속하는건 금지 됨
+//class Sqare: Rectangle {
+//}
 
 
 
