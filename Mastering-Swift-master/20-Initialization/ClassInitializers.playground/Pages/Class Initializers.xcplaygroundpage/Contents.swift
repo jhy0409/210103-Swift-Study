90d7872e77099258740452dc87024d97719fc7ef
jhy0409 <inooph@naver.com>
2021-06-24 09:40:41 +0900

    Swift 116강 - Class Initializers / 클래스 생성자

--- Mastering-Swift-master/20-Initialization/ClassInitializers.playground/Pages/Class Initializers.xcplaygroundpage/Contents.swift
+++ Mastering-Swift-master/20-Initialization/ClassInitializers.playground/Pages/Class Initializers.xcplaygroundpage/Contents.swift
@@ -28,15 +28,47 @@ import UIKit
  # Convenience Initializer
  ![convenience](convenience.png)
  */
+/*
+ - 클래스에서 사용하는 이니셜라이저
+ 1] 지정생성자 : 메인, 모든속성 초기화
+ 2] 간편생성자
+ 
+ ※ 클래스 : 생성자 실행 완료전 슈퍼클래스 - 1] 호출해야 함*
+          * -> Initializer Delegation
+ 
+ 1]의 생성 수는 제한 없지만 보통하나만 만듦.
+ - 기본생성자 사용 또는
+ - super class에서 1]상속 시 직접 구현필요 X
+    -> 그 외 최소 1개의 1]이 필요 (= 최소 1개의 모든속성 초기화하는 생성자가 필요)
+ 
+ 2] 다양한 초기화방법 구현위한 유틸리티 성격의 생성자
+ - 모든속성 초기화 강제X, 필요한 것만 초기화 후 1] 호출하여 나머지 초기화 함
+ 
+ - 이니셜라이저 상속
+ */
 
+class Position {
+    var x: Double
+    var y: Double
+    
+    init(x: Double, y: Double) { // 1]
+        self.x = x
+        self.y = y
+    }
+    
+    convenience init(x: Double) {
+        self.init(x: x, y: 0.0)
+    }
+}
 
+// 과제
+let pX = Position(x: 5) // 간편생성자 사용 2]
+pX.x
+pX.y
 
-
-
-
-
-
-
+let pXY = Position(x: 8, y: 9) // 지정생성자 사용 1]
+pXY.x
+pXY.y
 
 
 //: [Next](@next)
--- Mastering-Swift-master/20-Initialization/ClassInitializers.playground/Pages/Initializer Inheritance.xcplaygroundpage/Contents.swift
+++ Mastering-Swift-master/20-Initialization/ClassInitializers.playground/Pages/Initializer Inheritance.xcplaygroundpage/Contents.swift
@@ -26,6 +26,16 @@ import Foundation
 /*:
  # Initializer Inheritance
  */
+/*
+ ---------------- 생성자 상속 ----------------
+ 
+ 이니셜라이저 : 서브클래스로 상속되지 않음
+ 
+ 상속되는 규칙
+ 1. 서브클래스 모든값이 기본값으로 초기화 되어있고 1] 미구현시 super class의 모든 1]이 상속됨
+ 2. 서브클래스가 모든 1]을 상속 받았거나 오버라이딩 시 모든 2]가 상속됨
+ */
+
 class Figure {
    var name: String
 
@@ -36,21 +46,39 @@ class Figure {
    func draw() {
       print("draw \(name)")
    }
+    
+    convenience init() { // 해당클래스의 1] 호출 및 기본값 사용할 문자열 전달
+        self.init(name: "unknown")
+    }
 }
 
 class Rectangle: Figure {
-   var width: Double
-   var height: Double
+    var width: Double = 0.0
+    var height: Double = 0.0
+    
+    init(name: String, width: Double, height: Double) { // 1]
+        self.width = width
+        self.height = height
+        super.init(name: name) // 현재 클래스 속성 초기화 후 상위구현 호출
+    }
+    
+    override init(name: String) { // 1]-2 상속받은 생성자 오버라이딩, 1] 클래스 구현 후 상위클래스 호출
+        width = 0
+        height = 0
+        super.init(name: name)
+    }
+    
+    /*
+     1]-2 에러이유 - 상속계층 포함됐을 때 구현
+     - super 1] 호출
+     - 상위구현 호출 전 상속받은 name속성의 접근 불가능
+     */
+    
+    convenience init() { // Figure의 간편생성자 복붙, -> "동일한 클래스"의 다른 생성자 호출
+        // 슈퍼클래스의 생성자 직접호출 불가능해서 오버라이딩 안됨
+        self.init(name: "unknown")
+    }
 }
-
-
-
-
-
-
-
-
-
 
 
 //: [Next](@next)
