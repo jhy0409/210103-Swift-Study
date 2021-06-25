d1b41341a6025bdd96c9cffaffefa8b614012712
jhy0409 <inooph@naver.com>
2021-06-25 16:46:48 +0900

    Swift 125강 - Adding Subscripts / 익스텐션 → 서브스크립트 추가

--- Mastering-Swift-master/21-Extensions/AddingSubscripts.playground/Contents.swift
+++ Mastering-Swift-master/21-Extensions/AddingSubscripts.playground/Contents.swift
@@ -24,5 +24,33 @@ import UIKit
 /*:
  # Adding Subscripts
  */
+extension String {
+    subscript(idx: Int) -> String? {
+        guard (0..<count).contains(idx) else {
+            return nil
+        }
+        
+        let target = index(startIndex, offsetBy: idx)
+        return String(self[target]) // self expression : 형식에 구현된 다른 서브스크립트 호출
+    }
+}
 
+let str = "Swift"
+str[1]
+str[100]
+
+// subScript 전달값 제한 없음
+
+// 서브스크립트 : 날짜 컴포넌트 리턴
+extension Date {
+    subscript(component: Calendar.Component) -> Int? {
+        let cal = Calendar.current
+        return cal.component(component, from: self)
+    }
+}
+
+let today = Date()
+today[.year]
+today[.month]
+today[.day]
 
