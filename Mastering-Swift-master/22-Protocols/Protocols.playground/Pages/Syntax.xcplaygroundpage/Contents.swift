269a43ef9755fd47ac85f7fad18766effc3bd47f
jhy0409 <inooph@naver.com>
2021-06-25 17:17:40 +0900

    Swift 126강 - Protocol Syntax / 프로토콜 문법( 선언 및 채용)

--- Mastering-Swift-master/22-Protocols/Protocols.playground/Pages/Syntax.xcplaygroundpage/Contents.swift
+++ Mastering-Swift-master/22-Protocols/Protocols.playground/Pages/Syntax.xcplaygroundpage/Contents.swift
@@ -26,26 +26,62 @@ import UIKit
  ![define](define.png)
  ![protocol-inheritance](protocol-inheritance.png)
  */
+/*
+ 형식에서 공통적으로 제공하는 멤버 목록 // 자바, 씨샵의 인터페이스와 유사
+ 
+ [Protocol]                                 [Type]
+ - Declaration                            Implementation
+ - Declaration  -- Adopting Protocols* -> Implementation // * : 프로토콜을 따른다, 채용한다.
+ - Declaration                            Implementation
+ 
+ Protocol : 구현해야하는 멤버 선언돼 있음, Declarations(= 요구사항)
+ 실제 구현은 Type(형식에서)
+ 
+ ==========================
+ - 선언만 오고 구현은 X
+ - 상속지원, 클래스 상속문법과 동일
+ - 다중상속 허용
+ */
 
-
-
-
-
+protocol Something {
+    func doSomething()
+}
 
 /*:
  # Adopting Protocols
  ![adopting](adopting.png)
  */
+/*
+ 프로토콜 채용문법
+ 프로토콜 채용시 명시적으로 선언
+ 
+ - 클래스 : super class, Protocol같이 상속받으면 항상 슈퍼클래스 먼저 나열
+ */
 
-
-
-
-
+struct Size: Something {
+    func doSomething() { // 메소드 헤드만 프로토콜 선언과 일치,
+        // 메소드 바디는 자유롭게 구현가능
+    }
+}
 
 /*:
  # Class-Only Protocols
  ![class-only](class-only.png)
  */
+/*
+ 클래스 전용 프로토콜 선언 : 구조체, 열거형에서는 프로토콜 채용 불가능
+ */
 
+protocol SomethingObject: AnyObject, Something {
+}
 
+class Object: SomethingObject {
+    func doSomething() {
+    }
+}
 
+/*
+ struct Value: SomethingObject {
+ // errorMsg : 비 클래스 유형 'Value'는 클래스 프로토콜 'SomethingObject'를 준수 할 수 없습니다.
+ }
+ */
