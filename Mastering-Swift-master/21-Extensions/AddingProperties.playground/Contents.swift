265838f4df31781b71b115aea6a4680d3984aad1
jhy0409 <inooph@naver.com>
2021-06-25 11:16:26 +0900

    Swift 122강 - Adding Properties / 익스텐션으로 계산 속성을 추가

--- Mastering-Swift-master/21-Extensions/AddingProperties.playground/Contents.swift
+++ Mastering-Swift-master/21-Extensions/AddingProperties.playground/Contents.swift
@@ -24,19 +24,91 @@ import UIKit
 /*:
  # Adding Properties
  */
+/*
+ extension - 속성 추가 : 계산속성으로 제한
+ 
+ [추가 불가능]
+ - 저장속성
+ - 프로퍼티 옵저버(속성감시자)
+ - 형식에 존재하는 속성 오버라이딩
+ */
 
+extension Date {
+    var year: Int {
+        let cal = Calendar.current
+        return cal.component(.year, from: self) // 속성내부 날짜 인스턴스 접근
+    }
+    
+    var month: Int { // 과제
+        let cal = Calendar.current
+        return cal.component(.month, from: self)
+    }
+    
+    var weekday: String {
+        let cal = Calendar.current
+        let wdN = cal.component(.weekday, from: self)
+        print(wdN)
+        let yoil = getWeekday(wdN: wdN)
+        return yoil
+    }
+    
+    func getWeekday(wdN: Int) -> String {
+        let yoilKR = [ "","일", "월", "화", "수", "목", "금", "토"]
+        var returnYoil: String = ""
+        switch wdN {
+        case 1:
+            print(wdN)
+            returnYoil = yoilKR[1]
+        case 2:
+            print(wdN)
+            returnYoil = yoilKR[2]
+        case 3:
+            print(wdN)
+            returnYoil = yoilKR[3]
+            
+        case 4:
+            print(wdN)
+            returnYoil = yoilKR[4]
+        case 5:
+            print(wdN)
+            returnYoil = yoilKR[5]
+        case 6:
+            returnYoil = yoilKR[6]
+        case 7:
+            returnYoil = yoilKR[7]
+        default:
+            break
+        }
+        return returnYoil
+    }
+}
 
+let today = Date()
 
+today.year
+today.month
+today.weekday
 
+/*
+ let date = NSDate() // 현재 시간 가져오기
+ let formatter = DateFormatter()
+ formatter.locale = Locale(identifier: "ko") // 로케일 변경
+ formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEEE"
+ print("현재시간: " + formatter.string(from: date as Date))
+ */
 
+extension Double {
+    var radianValue: Double {
+        return (Double.pi * self) / 180.0
+        // self : double형식에 저장된 실수 접근
+    }
+    
+    var degreeValue: Double { // radian -> degree
+        return self * 180.0 / Double.pi
+    }
+}
 
-
-
-
-
-
-
-
-
-
-
+let dv = 45.0
+dv.radianValue
+dv.radianValue.degreeValue
+// 같은 double형이므로 연달아 호출 가능
