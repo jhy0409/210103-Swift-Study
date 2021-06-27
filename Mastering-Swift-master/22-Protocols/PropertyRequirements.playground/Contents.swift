b13e8a5593b8db45a67535b7de0241c1b4acf873
jhy0409 <inooph@naver.com>
2021-06-27 17:03:30 +0900

    Swift 127강 - Property Requirements / 속성 요구사항

--- Mastering-Swift-master/22-Protocols/PropertyRequirements.playground/Contents.swift
+++ Mastering-Swift-master/22-Protocols/PropertyRequirements.playground/Contents.swift
@@ -25,15 +25,55 @@ import UIKit
  # Property Requirements
  ![property](property.png)
  */
+/*
+ 프로토콜에서 속성을 선언하고 형식에서 요구사항을 구현
 
+ - var 키워드 용법
+ - 가변성 선언
+ - static 키워드와 class 키워드
+ 
+ ============================================
+ 
+ 프로토콜에서는 항상 var키워드로 선언
+    1. 가변성과 상관없이 선언멤버가 속성인걸 나타냄
+    2. 가변성 결정 : get, set이 결정
+        - 둘 다 있음 : 읽기, 쓰기 가능
+        - get만    : 읽기전용 속성
+ */
 
+protocol Figure {
+    static var name: String { get set }
+}
 
+struct Rectangle: Figure {
+    // 프로토콜에 선언된 속성을 추가, 속성이름은 반드시 name, 형식은 String
+    // get만 있으므로 읽기만 가능하면 요구사항 충족됨
+    static var name = "Rect" // 상수 저장속성 : 값 쓰기(set) 불가능
+}
 
+struct Triangle: Figure {
+    static var name = "Triangle" // 최소요구사항, 꼭 읽기전용으로 선언 X
+}
 
+//struct Circle: Figure {
+//    static var name: String { // 계산속성으로 선언, set이 있을 때 : get, set블록 필요
+//        get {
+//            return "Circle"
+//        }
+//        set {
+//        }
+//    }
+//}
 
-
-
-
-
-
+class Circle: Figure {
+    // 형식속성, 이름, 자료형, 가변성 동일 -> 프로토콜 요구사항 충족, 오버라이딩 가능
+    class var name: String { // 계산속성으로 선언, set이 있을 때 : get, set블록 필요
+        // static : 상속되지만, overriding 불가능
+        get {
+            return "Circle"
+        }
+        set {
+        }
+    }
+}
 
