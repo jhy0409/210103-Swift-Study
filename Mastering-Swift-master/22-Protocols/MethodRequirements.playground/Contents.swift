d208888be79941a4358a73fd7dd992f2734e9813
jhy0409 <inooph@naver.com>
2021-06-27 17:20:34 +0900

    Swift 128강 - Method Requirements / 프로토콜 메소드 선언, 구현

--- Mastering-Swift-master/22-Protocols/MethodRequirements.playground/Contents.swift
+++ Mastering-Swift-master/22-Protocols/MethodRequirements.playground/Contents.swift
@@ -25,4 +25,46 @@ import UIKit
  # Method Requirements
  ![method](method.png)
  */
+/*
+ 프로토콜에서 메소드를 선언하고 형식에서 요구사항을 구현
 
+ - mutating 메소드 선언
+ - static 키워드와 class 키워드
+ 
+ [프로토콜 메소드 작성]
+ - 메소드 헤드만 작성
+ - 타입메소드  : static키워드 추가
+ - mutating : 프로토콜을 값형식이 채용, 메소드 내부에서 속성값 변경
+    -> 꼭 값형식 전용이란게 아니라 메소드에서 속성을 변경할 수 있어야 한다.
+       참조형식에서도 문제없이 사용 가능
+ */
+
+protocol Resettable {
+    static func reset()
+}
+
+class Size_C: Resettable {
+    var width = 0.0
+    var height = 0.0
+
+    func reset() { // 메소드 이름, 파라미터 형식, 리턴형 일치, 메소드 바디는 자유롭게 구현
+        width = 0.0
+        height = 0.0
+    }
+    
+    class func reset() { // 타입형식, 오버라이딩 가능
+    }
+}
+
+/*
+ struct Size: Resettable {
+ var width = 0.0
+ var height = 0.0
+ 
+ mutating func reset() { // 메소드 이름, 파라미터 형식, 리턴형 일치, 메소드 바디는 자유롭게 구현
+ width = 0.0
+ height = 0.0
+ }
+ // mutating : 값형식 인스턴스에서 속성값 바꾸기 -> 프로토콜에서도 mutating 추가해야함
+ }
+ */
