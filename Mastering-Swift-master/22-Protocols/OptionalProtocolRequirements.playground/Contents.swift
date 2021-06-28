88f50f6c4013eeb5e5c6206d026d05c2db7d5093
jhy0409 <inooph@naver.com>
2021-06-28 09:54:11 +0900

    Swift 133강 - Optional Requirements / 선택적 요구사항 선언

--- Mastering-Swift-master/22-Protocols/OptionalProtocolRequirements.playground/Contents.swift
+++ Mastering-Swift-master/22-Protocols/OptionalProtocolRequirements.playground/Contents.swift
@@ -25,13 +25,52 @@ import UIKit
  # Optional Protocol Requirements
  ![optional](optional.png)
  */
-
-protocol Drawable {
-   var strokeWidth: Double { get set }
-   var strokeColor: UIColor { get set }
-   func draw()
-   func reset()
+/*
+ 옵셔널 형식이 아닌 선택적이란 뜻
+ 
+ 1] objc Attribute
+ - 스위프트 코드 objective-C에서 사용할 수 있게 해주는 코드
+ - @문자로 시작
+ - 선언이나 형식에 부가적 속성(attribute) 선언할 때 사용
+ 
+ 2] optional modifier
+ - 선택적 멤버 선언시 사용
+ 
+ * = objective C
+ */
+protocol Drawable_1org { // 현재 선언된 모든 속성이 필수사항
+    var strokeWidth: Double { get set }
+    var strokeColor: UIColor { get set }
+    func draw()
+    func reset()
 }
 
+@objc protocol Drawable_2opt { // 현재 선언된 draw 메소드만 필수사항
+    @objc optional var strokeWidth: Double { get set }
+    @objc optional var strokeColor: UIColor { get set }
+    func draw()
+    @objc optional func reset()
+}
 
+/*
+    1]로 선언한 프로토콜 -> objc 프로토콜
+        - 클래스에서만 채용할 수 있음 (애니오브젝트 자동 상속되므로)
+        - *과 swift간 호환성 위해 필요한 제한 (*는 구조체, 열거형에서 프로토콜 미지원)
+ 
+    - 선택적 멤버 접근 : 옵셔널 체이닝
+        -> 선택적 멤버로 선언 : 옵셔널 형식,
+        -> non-O -> 존재하지 않는 멤버의 접근이므로 선택적 멤버 옵셔널 형식으로 대체 됨
+ */
 
+class Rectangle: Drawable_2opt {
+    func draw() {
+    }
+}
+
+let r: Drawable_2opt = Rectangle()
+r.draw()
+// 선택적 멤버(=옵셔널 형식) 접근시 옵셔널 체이닝 필요
+r.strokeWidth
+r.strokeColor
+
+r.reset?()
