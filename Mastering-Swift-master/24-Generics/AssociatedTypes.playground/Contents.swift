440ccf2f17ba7f1c0c4945fe31a8cf4c98eb89f3
jhy0409 <inooph@naver.com>
2021-07-01 09:27:38 +0900

    Swift 142강 - Associated Types / 연관 형식(프로토콜내 실제 형식 대체)

--- Mastering-Swift-master/24-Generics/AssociatedTypes.playground/Contents.swift
+++ Mastering-Swift-master/24-Generics/AssociatedTypes.playground/Contents.swift
@@ -25,19 +25,45 @@ import UIKit
  # Associated Types
  ![associatedtype](associatedtype.png)
  */
-
+/*
+ 프로토콜 내에서 실제 형식으로 대체되는 연관 형식을 선언하는 방법에 대해 공부합니다.
+ 
+ - 제네릭 프로토콜
+ - Associated Types
+ - typealias 를 통한 실제 형식 선언
+ - 연관 형식에 제약 추가
+ 
+ 프로토콜에서 사용하는 플레이스 홀더 형식, 요구사항 선언은 아님
+ */
 protocol QueueCompatible {
-   func enqueue(value: Int)
-   func dequeue() -> Int?
+    associatedtype Element: Equatable
+    func enqueue(value: Element)
+    func dequeue() -> Element?
 }
 
+/*:
+ ## typealias AssociatedTypeName = Type
+ */
+/*
+ 연관형식 대체하는 실제형식 -> 프로토콜 채용형식에 따라 결정됨
+ */
 
+class IntegerQueue: QueueCompatible {
+    typealias Element = Int // 주로 생략하는 편
+    
+    func enqueue(value: Int) {
+    }
+    func dequeue() -> Int? {
+        return 0
+    }
+}
 
-
-
-
-
-
-
-
-
+// 연관형식 생략 : 사용된 실제형식으로 연관형식 추론하기때문에 생략해도 문제 X
+class DoubleQueue: QueueCompatible {
+    func enqueue(value: Double) {
+    }
+    
+    func dequeue() -> Double? {
+        return 0.0
+    }
+}
