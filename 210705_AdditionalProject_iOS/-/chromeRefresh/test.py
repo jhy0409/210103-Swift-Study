197eac4bcb816e7fcd759c414290499fb4f86f87
jhy0409 <inooph@naver.com>
2021-07-05 17:53:37 +0900

    탭바컨트롤러 추가

---
+++ 210705_AdditionalProject_iOS/-/chromeRefresh/test.py
@@ -0,0 +1,17 @@
+import time
+# import pyautogui # 설치했는데 아래 내용으로 오류가 뜸
+'''
+========== 오류 내용 ==========
+Traceback (most recent call last):
+  File "test.py", line 2, in <module>
+    import pyautogui
+ModuleNotFoundError: No module named 'pyautogui'
+Program ended with exit code: 1
+'''
+
+i = 1
+
+print(i)
+time.sleep(1)
+i += 1
+print(i)
