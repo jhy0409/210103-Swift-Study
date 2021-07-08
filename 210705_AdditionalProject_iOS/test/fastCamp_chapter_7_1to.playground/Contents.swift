70e7b40e1c1fc7b05b28b89b09e9feb62dca3249
jhy0409 <inooph@naver.com>
2021-07-08 15:45:08 +0900

    [인강복습] 패캠 / 챕터 7-2 sturcture 코드개선 1(Location struct, name) tuple array

--- 210705_AdditionalProject_iOS/test/fastCamp_chapter_7_1to.playground/Contents.swift
+++ 210705_AdditionalProject_iOS/test/fastCamp_chapter_7_1to.playground/Contents.swift
@@ -18,13 +18,50 @@ import UIKit
 // 문제 : 가장 가까운 편의점 찾기
 // 맨처음 시작시 코드
 
+/*
+ 코드 개선 전(struct 미사용)
+ 
+ // 현재 스토어 위치들
+ let store1 = (x: 3, y: 5, name: "gs편의점")
+ let store2 = (x: 4, y: 6, name: "seven편의점")
+ let store3 = (x: 1, y: 7, name: "cu편의점")
+ 
+ // 거리 구하는 함수
+ func distance(current: (x: Int, y: Int), target: (x: Int, y: Int)) -> Double {
+ let distanceX = Double(target.x - current.x) // 거리차이 : 가로
+ let distanceY = Double(target.y - current.y) // 거리차이 : 세로
+ let distance = sqrt(distanceX * distanceX + distanceY * distanceY) // 피타고라스
+ return distance
+ }
+ 
+ // 가장 가까운 편의점 프린트하는 함수
+ func printClosestStore(currentLocation: (x: Int, y: Int), stores:[(x: Int, y: Int, name: String)]) { // 사람의 현재위치, 편의점 목록을 받는다.
+ var closestStoreName = "" // 근처 편의점 이름 저장할 변수선언
+ var closestStoreDistance = Double.infinity // 근처매장거리 - 최소값 대조위한 변수선언
+ 
+ for store in stores {
+ let distanceToStore = distance(current: currentLocation, target: (x: store.x, y: store.y))
+ // 현위치의 튜플을 넘김, 현재 순서의 배열 내 매장위치 튜플값을 넘김
+ closestStoreDistance = min(distanceToStore, closestStoreDistance)
+ // 현재배열 순서에서 계산된 거리(더블 최대값)중에서 최소값으로 재할당
+ // 두번째 실행 시 첫번째 편의점 값에서 대조함!
+ 
+ if closestStoreDistance == distanceToStore { // 최소값이 계산된 값과 같으면
+ closestStoreName = store.name // 근처 편의점 이름을 현재 편의점이름으로 정의한다.
+ }
+ }
+ print("Closest store: \(closestStoreName)")
+ }
+ */
+
 // 현재 스토어 위치들
-let store1 = (x: 3, y: 5, name: "gs편의점")
-let store2 = (x: 4, y: 6, name: "seven편의점")
-let store3 = (x: 1, y: 7, name: "cu편의점")
+let store1 = (loc: Location(x: 3, y: 5), name: "gs편의점")
+let store2 = (loc: Location(x: 4, y: 6), name: "seven편의점")
+//let store3 = (x: 1, y: 7, name: "cu편의점")
+let store3 = (loc: Location(x: 1, y: 7), name: "cu편의점")
 
 // 거리 구하는 함수
-func distance(current: (x: Int, y: Int), target: (x: Int, y: Int)) -> Double {
+func distance(current: Location, target: Location) -> Double {
     let distanceX = Double(target.x - current.x) // 거리차이 : 가로
     let distanceY = Double(target.y - current.y) // 거리차이 : 세로
     let distance = sqrt(distanceX * distanceX + distanceY * distanceY) // 피타고라스
@@ -32,12 +69,12 @@ func distance(current: (x: Int, y: Int), target: (x: Int, y: Int)) -> Double {
 }
 
 // 가장 가까운 편의점 프린트하는 함수
-func printClosestStore(currentLocation: (x: Int, y: Int), stores:[(x: Int, y: Int, name: String)]) { // 사람의 현재위치, 편의점 목록을 받는다.
+func printClosestStore(currentLocation: Location, stores:[(loc: Location, name: String)]) { // 사람의 현재위치, 편의점 목록을 받는다.
     var closestStoreName = "" // 근처 편의점 이름 저장할 변수선언
     var closestStoreDistance = Double.infinity // 근처매장거리 - 최소값 대조위한 변수선언
     
     for store in stores {
-        let distanceToStore = distance(current: currentLocation, target: (x: store.x, y: store.y))
+        let distanceToStore = distance(current: currentLocation, target: store.loc)
         // 현위치의 튜플을 넘김, 현재 순서의 배열 내 매장위치 튜플값을 넘김
         closestStoreDistance = min(distanceToStore, closestStoreDistance)
         // 현재배열 순서에서 계산된 거리(더블 최대값)중에서 최소값으로 재할당
@@ -51,8 +88,8 @@ func printClosestStore(currentLocation: (x: Int, y: Int), stores:[(x: Int, y: In
 }
 
 // Stores Array 세팅, 내위치 세팅
-let myLocation = (x: 2, y: 2)
-let stores = [store1, store2, store3]
+let myLocation = Location(x: 2, y: 2)
+let stores = [store1, store2, store3] // Location struct와 편의점 이름을 가진 편의점 튜플 배열
 
 // printClosetStore 함수 이용해서 현재 가장 가까운 스토어 출력
 printClosestStore(currentLocation: myLocation, stores: stores)
@@ -64,4 +101,9 @@ printClosestStore(currentLocation: myLocation, stores: stores)
 struct Location {
     let x: Int
     let y: Int
+}
+
+struct Store {
+    let loc: Location
+    let name: String
 }
