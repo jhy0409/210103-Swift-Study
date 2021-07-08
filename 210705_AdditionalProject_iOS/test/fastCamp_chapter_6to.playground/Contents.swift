1b4a895adb15344866b3ae2aec3a6f7ee90d1887
jhy0409 <inooph@naver.com>
2021-07-08 11:56:56 +0900

    [인강복습] 패캠 / 챕터 6-8 클로저 : 이름이 없는 메소드(함수)

--- 210705_AdditionalProject_iOS/test/fastCamp_chapter_6to.playground/Contents.swift
+++ 210705_AdditionalProject_iOS/test/fastCamp_chapter_6to.playground/Contents.swift
@@ -53,8 +53,56 @@ printDic(dic: myDic)
 // chapter 6-7 실습
 // array: 순서가 있는 아이템, dictionary: 키로 값 관리, Set: 중복없는 유니크한 아이템
 // 공통 property : isEmpty, count, inser, remove
+print("\n\(doubleLine)\nchapter 6-7 실습 : Set\n\(doubleLine)")
 var someArray: Array<Int> = [1, 2, 3, 3, 3]
-print(someArray.count) // print : 5
+print("someArray count\t: \(someArray.count)") // print : 5
 
 var someSet: Set<Int> = [1, 2, 2, 2, 3, 3, 3]
-print(someSet.count) // print : 3
+print("someSet count\t: \(someSet.count)") // print : 3
+
+// chapter 6-8 클로저 : 이름이 없는 메소드
+print("\n\(doubleLine)\nchapter 6-8 클로저 : 이름이 없는 메소드\n\(doubleLine)")
+
+// 1. 정식문법
+var gopagiClosure1_defSytx: (Int, Int) -> Int = { (a: Int, b: Int) -> Int in
+    return a * b
+}
+
+// 2. 생략1
+var gopagiClosure2_shrt1: (Int, Int) -> Int = { (a, b) in
+    return a * b
+}
+/*
+ 1. 타입 파라미터, 소괄호 생략
+ 2. 반환형 생략
+ 3. 파라미터 인덱스로 바꿀 수 있음
+ 4. 리턴 생략
+ */
+
+// 2. 생략2
+var gopagiClosure2_shrt2: (Int, Int) -> Int = { return $0 * $1 }
+
+// 2. 생략3
+var gopagiClosure2_shrt3: (Int, Int) -> Int = { $0 * $1 }
+
+let num1 = 3
+let num2 = 2
+
+gopagiClosure1_defSytx(num1, num2)
+gopagiClosure2_shrt1(num1, num2)
+gopagiClosure2_shrt2(num1, num2)
+gopagiClosure2_shrt3(num1, num2)
+
+func operateTwoNum(a: Int, b: Int, operation: (Int, Int) -> Int) -> Int {
+    let result = operation(a, b)
+    return result
+}
+
+var addClosure: (Int, Int) -> Int = { a, b in
+    return a + b
+}
+operateTwoNum(a: num1, b: num2, operation: gopagiClosure2_shrt1)
+operateTwoNum(a: num1, b: num2, operation: addClosure)
+operateTwoNum(a: 20, b: 2) { a, b in
+    return a / b
+}
