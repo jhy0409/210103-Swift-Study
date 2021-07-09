72cab9d62c36df2b4804def9e9b30efd7b290b13
jhy0409 <inooph@naver.com>
2021-07-09 17:45:03 +0900

    [인강복습] 패캠 / 챕터 7-9~10 - 개념 및 실습, 메소드 확장

--- 210705_AdditionalProject_iOS/test/fastCamp_chapter_7_5to.playground/Contents.swift
+++ 210705_AdditionalProject_iOS/test/fastCamp_chapter_7_5to.playground/Contents.swift
@@ -2,50 +2,50 @@ import UIKit
 /*
  // -------- chapter 7-5강
  struct Person {
-     var firstName: String   // Stored Property(저장속성)
-     var lastName: String    // Stored Property(저장속성)
-     
-     /*
-      Computed Property (계산속성) var만 가능
-      1-1. read only 값 세팅 불가능
-      1-2. 세팅 가능하게 -> get, set 삽입
-      
-      2. Type Property : 생성된 인스턴스 상관없이 타입자체의 속성 정하고 싶을 때 사용하는 것
-      */
-     var fullName: String {
-         get {
-             return "\(firstName) \(lastName)"
-         }
-         set {
-             if let firstName = newValue.components(separatedBy: " ").first {
-                 self.firstName = firstName
-             }
-             // newValue : 할당되는 새로운 값
-             if let lastName = newValue.components(separatedBy: " ").last {
-                 self.lastName = lastName
-             }
-         }
-     }
-     static let isAlien: Bool = false
+ var firstName: String   // Stored Property(저장속성)
+ var lastName: String    // Stored Property(저장속성)
+ 
+ /*
+ Computed Property (계산속성) var만 가능
+ 1-1. read only 값 세팅 불가능
+ 1-2. 세팅 가능하게 -> get, set 삽입
+ 
+ 2. Type Property : 생성된 인스턴스 상관없이 타입자체의 속성 정하고 싶을 때 사용하는 것
+ */
+ var fullName: String {
+ get {
+ return "\(firstName) \(lastName)"
  }
-
+ set {
+ if let firstName = newValue.components(separatedBy: " ").first {
+ self.firstName = firstName
+ }
+ // newValue : 할당되는 새로운 값
+ if let lastName = newValue.components(separatedBy: " ").last {
+ self.lastName = lastName
+ }
+ }
+ }
+ static let isAlien: Bool = false
+ }
+ 
  var person = Person(firstName: "Jason", lastName: "1-1")
  person.firstName
  person.lastName
-
+ 
  person.firstName = "Jim"
  person.lastName = "1-2"
-
+ 
  person.firstName
  person.lastName
-
+ 
  person.fullName
  person.fullName = "Jay P_1-3"
-
+ 
  person.firstName
  person.lastName
  Person.isAlien
-
+ 
  // -------- chapter 7-6강 ~ 7-7강
  let doubleLine = "========================================="
  struct Person {
@@ -122,15 +122,15 @@ import UIKit
  
  // 경우에 따른 사용
  1. Computed Property
-    - setter 필요
+ - setter 필요
  
  2. Method
-    - setter 미사용
-    - DB access나 File io가 필요할 때
+ - setter 미사용
+ - DB access나 File io가 필요할 때
  */
 
-// -------- chapter 7-9강
 
+// -------- chapter 7-9강
 struct Lecture {
     var title: String
     var maxStudents: Int = 10
@@ -152,7 +152,6 @@ struct Lecture {
 
 var lec = Lecture(title: "1. iOS Basic")
 
-
 lec.remainSeats()
 lec.register()
 lec.register()
@@ -161,3 +160,46 @@ lec.remainSeats()
 
 Lecture.target // type property
 Lecture.소속한원이름() // type method
+
+// -------- chapter 7-10강 struct extension
+
+struct Math {
+    static func abs(value: Int) -> Int {
+        if value > 0 {
+            return value
+        } else {
+            return -value
+        }
+    }
+}
+
+Math.abs(value: -20)
+// 코드가 숨겨져있거나 사용하는 코드 바로밑에서 만들어 쓰고 싶을 때
+// 같은 struct안에 여러 코드 통합누적 시 충돌날 수 있음
+// 제곱, 반값
+extension Math {
+    static func square(value: Int) -> Int {
+        return value * value
+    }
+    static func half(value: Int) -> Int {
+        return value/2
+    }
+}
+
+Math.square(value: 5)
+Math.half(value: 20)
+
+var value: Int = 3
+// 제곱, 반값
+
+extension Int {
+    func square() -> Int {
+        return self * self
+    }
+    func half() -> Int {
+        return self/2
+    }
+}
+
+value.square()
+value.half()
