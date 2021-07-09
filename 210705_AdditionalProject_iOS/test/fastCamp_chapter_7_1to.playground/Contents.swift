0123b4c3461a451b88de6ee903e41c7c8c6643c1
jhy0409 <inooph@naver.com>
2021-07-09 14:56:38 +0900

    [인강복습] 패캠 / 챕터 7-4 Protocol 프로토콜

--- 210705_AdditionalProject_iOS/test/fastCamp_chapter_7_1to.playground/Contents.swift
+++ 210705_AdditionalProject_iOS/test/fastCamp_chapter_7_1to.playground/Contents.swift
@@ -183,6 +183,7 @@ import UIKit
  */
 
 // chapter 7-3 실습
+let doubleLineStr = "=============================="
 
 /*
  실습문제
@@ -191,33 +192,69 @@ import UIKit
  2. 강의 어레이, 강사이름 전달 -> 해당강사 강의이름 출력하는 함수 만들기
  3. 강의 3개 만들고 강사 이름으로 강의 찾기
  */
-let doubleLineStr = "=============================="
 /*
  // ------------ 내 풀이 ------------
+ struct Lecture {
+ let subject: String
+ let gangsa: String
+ var studentNum: Int
+ }
+ 
+ func printLecture(lec: [Lecture], gangsaIrum: String) {
+ //    var lectureName = ""
+ //    for tmp in lec {
+ //        if(gangsaIrum == tmp.gangsa) { // 받아오는 값 == 현재강사 이름
+ //            lectureName = tmp.gangsa
+ //        }
+ //    }
+ 
+ // 클로저 활용1 - 축약 전 정식문법
+ //let lectureName = lectures.first{ (lec) -> Bool in
+ //    return lec.gangsa == gangsaIrum
+ //}?.subject ?? ""
+ 
+ // 클로저 활용2 - 축약
+ let lectureName = lectures.first{ $0.gangsa == gangsaIrum }?.subject ?? ""
+ let lc = lectures.first{ $0.gangsa == gangsaIrum } ?? nil
+ 
+ if lectureName.count == 0 {
+ print("해당 강사의 강의가 존재하지 않습니다.")
+ return
+ }
+ print("강의이름 : \(lectureName)")
+ }
+ 
+ let gangwi_1 = Lecture(subject: "1강의", gangsa: "1강사", studentNum: 1111)
+ let gangwi_2 = Lecture(subject: "2강의", gangsa: "2강사", studentNum: 2222)
+ let gangwi_3 = Lecture(subject: "3강의", gangsa: "3강사", studentNum: 3333)
+ 
+ let lectures: [Lecture] = [gangwi_1, gangwi_2, gangwi_3]
+ 
+ printLecture(lec: lectures, gangsaIrum: "1강사")
  */
-struct Lecture {
+
+
+// chapter 7-4 실습
+struct Lecture: CustomStringConvertible {
+    // 설명형으로 쓸 때 CustomStringConvertible 프로토콜 활용
+    var description: String {
+        return "Title \(subject), gangsa: \(gangsa)"
+    }
+    
     let subject: String
     let gangsa: String
     var studentNum: Int
 }
 
 func printLecture(lec: [Lecture], gangsaIrum: String) {
-    //    var lectureName = ""
-    //    for tmp in lec {
-    //        if(gangsaIrum == tmp.gangsa) { // 받아오는 값 == 현재강사 이름
-    //            lectureName = tmp.gangsa
-    //        }
-    //    }
     
-    // 클로저 활용1 - 축약 전 정식문법
-    //let lectureName = lectures.first{ (lec) -> Bool in
-    //    return lec.gangsa == gangsaIrum
-    //}?.subject ?? ""
+    var lectureName = ""
     
-    // 클로저 활용2 - 축약
-    let lectureName = lectures.first{ $0.gangsa == gangsaIrum }?.subject ?? ""
-    let lc = lectures.first{ $0.gangsa == gangsaIrum } ?? nil
-    
+    for lc in lec {
+        if gangsaIrum == lc.gangsa {
+            lectureName = lc.gangsa
+        }
+    }
     if lectureName.count == 0 {
         print("해당 강사의 강의가 존재하지 않습니다.")
         return
@@ -231,4 +268,5 @@ let gangwi_3 = Lecture(subject: "3강의", gangsa: "3강사", studentNum: 3333)
 
 let lectures: [Lecture] = [gangwi_1, gangwi_2, gangwi_3]
 
-printLecture(lec: lectures, gangsaIrum: "1강사")
+printLecture(lec: lectures, gangsaIrum: "5강사")
+print(gangwi_1)
