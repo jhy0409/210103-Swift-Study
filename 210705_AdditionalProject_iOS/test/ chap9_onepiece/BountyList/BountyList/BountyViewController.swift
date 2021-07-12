e032eef04c318462d13bca2d2fca3a66a9e23873
jhy0409 <inooph@naver.com>
2021-07-12 16:04:47 +0900

    [인강복습] 패캠 / 챕터 9-5. 커스텀 테이블뷰 셀 만들기

--- 210705_AdditionalProject_iOS/test/ chap9_onepiece/BountyList/BountyList/BountyViewController.swift
+++ 210705_AdditionalProject_iOS/test/ chap9_onepiece/BountyList/BountyList/BountyViewController.swift
@@ -8,18 +8,26 @@
 import UIKit
 
 class BountyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    let nameList = ["brook", "chopper", "franky", "luffy", "nami", "robin", "sanji", "zoro"]
+    let bountyList = [33000000,50,44000000,300000000,16000000,80000000,77000000,120000000]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return bountyList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell else {
+            return UITableViewCell()
+        }
+        let img = UIImage(named: "\(nameList[indexPath.row]).jpg")
+        cell.imageView?.image = img
+        cell.nameLabel.text = nameList[indexPath.row]
+        cell.bountyLabel.text = "\(bountyList[indexPath.row])"
         return cell
     }
     
@@ -27,4 +35,10 @@ class BountyViewController: UIViewController, UITableViewDataSource, UITableView
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("--->\(indexPath.row)")
     }
+}
+
+class ListCell: UITableViewCell {
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var bountyLabel: UILabel!
 }
