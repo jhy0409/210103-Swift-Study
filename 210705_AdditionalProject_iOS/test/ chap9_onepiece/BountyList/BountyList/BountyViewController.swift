613f4afb543d376860311ff07cfad5c3f29380e4
jhy0409 <inooph@naver.com>
2021-07-12 15:03:53 +0900

    [인강복습] 패캠 / 챕터 9-3~4. 테이블뷰 프로토콜 실습

--- 210705_AdditionalProject_iOS/test/ chap9_onepiece/BountyList/BountyList/BountyViewController.swift
+++ 210705_AdditionalProject_iOS/test/ chap9_onepiece/BountyList/BountyList/BountyViewController.swift
@@ -7,23 +7,24 @@
 
 import UIKit
 
-class BountyViewController: UIViewController {
-
+class BountyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
+    }
+    
+    // 클릭됐을 때 UITableViewDelegate
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("--->\(indexPath.row)")
+    }
 }
