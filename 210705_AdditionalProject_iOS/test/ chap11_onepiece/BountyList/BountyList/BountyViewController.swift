8f2cad1375582a9f73fd88db1ff57d4d26d8448d
jhy0409 <inooph@naver.com>
2021-07-13 14:46:19 +0900

    [인강복습] 패캠 / 챕터 11-3. CollectionView 실습 02 DataSource, Delegate

--- 210705_AdditionalProject_iOS/test/ chap11_onepiece/BountyList/BountyList/BountyViewController.swift
+++ 210705_AdditionalProject_iOS/test/ chap11_onepiece/BountyList/BountyList/BountyViewController.swift
@@ -7,7 +7,8 @@
 
 import UIKit
 
-class BountyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class BountyViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
     // MVVM
     
     // Model
@@ -45,26 +46,46 @@ class BountyViewController: UIViewController, UITableViewDataSource, UITableView
         // Do any additional setup after loading the view.
     }
     
-    // UITableViewDataSource
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    // UICollectionViewDataSource : 몇개를 보여줄지, 셀은 어떻게 표현?
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return viewModel.numOfBountyInfoList
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell else {
-            return UITableViewCell()
-        }
-        let bountyInfo = viewModel.bountyInfo(at: indexPath.row)
+    // UICollectionViewDelegate : 셀 클릭 시 동작
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GridCell", for: indexPath) as? GridCell else { return UICollectionViewCell()}
+        let bountyInfo = viewModel.bountyInfo(at: indexPath.item)
         cell.update(info: bountyInfo)
+        
         return cell
     }
     
-    // UITableViewDelegate
-    func tableView(_ tableView: UITableView, didHighlightRowAt indexPath: IndexPath) {
-        print("--> \(indexPath.row)")
-        performSegue(withIdentifier: "showDetail", sender: indexPath.row)
+    // UICollectionViewDelegateFlowLayout : 셀 사이즈 계산 (목표: 디바이스마다 일관적인 디자인 보여주기 위해)
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("-->\(indexPath.item)")
+        performSegue(withIdentifier: "showDetail", sender: indexPath.item)
     }
+    
+//    // UITableViewDataSource
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return viewModel.numOfBountyInfoList
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//
+//        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell else {
+//            return UITableViewCell()
+//        }
+//        let bountyInfo = viewModel.bountyInfo(at: indexPath.row)
+//        cell.update(info: bountyInfo)
+//        return cell
+//    }
+//
+//    // UITableViewDelegate
+//    func tableView(_ tableView: UITableView, didHighlightRowAt indexPath: IndexPath) {
+//        print("--> \(indexPath.row)")
+//        performSegue(withIdentifier: "showDetail", sender: indexPath.row)
+//    }
 }
 
 class ListCell: UITableViewCell {
@@ -79,7 +100,17 @@ class ListCell: UITableViewCell {
     }
 }
 
-
+class GridCell: UICollectionViewCell {
+    @IBOutlet weak var ImgView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var bountyLabel: UILabel!
+    
+    func update(info: BountyInfo) {
+        ImgView.image = info.image
+        nameLabel.text = info.name
+        bountyLabel.text = "\(info.bounty)"
+    }
+}
 
 class BountyViewModel {
     let bountyInfoList: [BountyInfo] = [
