2c0bcc00af63b24932d2ec4d55d688f278bf2e89
jhy0409 <inooph@naver.com>
2021-07-13 17:43:07 +0900

    [인강복습] 패캠 / 챕터 11-8. 뷰속성을 이용한 Animation 실습

--- 210705_AdditionalProject_iOS/test/ chap11_onepiece/BountyList/BountyList/DetailViewController.swift
+++ 210705_AdditionalProject_iOS/test/ chap11_onepiece/BountyList/BountyList/DetailViewController.swift
@@ -44,23 +44,43 @@ class DetailViewController: UIViewController {
     }
     
     private func prepareAnimation() {
-        nameLabelCenterX.constant = view.bounds.width
-        bountyLabelCenterX.constant = view.bounds.width
+        //nameLabelCenterX.constant = view.bounds.width
+        //bountyLabelCenterX.constant = view.bounds.width
+        
+        nameLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: 0).scaledBy(x: 3, y: 3).rotated(by: 180)
+        
+        bountyLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: 0).scaledBy(x: 3, y: 3).rotated(by: 180)
+        
+        nameLabel.alpha = 0
+        bountyLabel.alpha = 0
     }
     
     private func showAnimation() {
-        nameLabelCenterX.constant = 0
-        bountyLabelCenterX.constant = 0
+        //nameLabelCenterX.constant = 0
+        //bountyLabelCenterX.constant = 0
+        //
+        //UIView.animate(withDuration: 0.3,
+        //               delay: 0.1,
+        //               options: .curveEaseIn,
+        //               animations: { self.view.layoutIfNeeded() }, completion: nil)
+        //
+        //UIView.animate(withDuration: 0.3, delay: 0.1, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: { self.view.layoutIfNeeded() }, completion: nil)
+        //
+        //UIView.transition(with: ImgView, duration: 0.3, options: .transitionFlipFromLeft, animations: nil, completion: nil)
         
-//        UIView.animate(withDuration: 0.3,
-//                       delay: 0.1,
-//                       options: .curveEaseIn,
-//                       animations: { self.view.layoutIfNeeded() }, completion: nil)
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: {
+            self.nameLabel.transform = CGAffineTransform.identity // 변형 전 원래
+            self.nameLabel.alpha = 1
+        }, completion: nil)
         
-        UIView.animate(withDuration: 0.3, delay: 0.1, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: { self.view.layoutIfNeeded() }, completion: nil)
+        UIView.animate(withDuration: 1, delay: 0.2, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: {
+            self.bountyLabel.transform = CGAffineTransform.identity
+            self.bountyLabel.alpha = 1
+        }, completion: nil)
         
         UIView.transition(with: ImgView, duration: 0.3, options: .transitionFlipFromLeft, animations: nil, completion: nil)
-    }
+        
+        }
     
     func updateUI() {
         if let bountyInfo = viewModel.bountyInfo {
