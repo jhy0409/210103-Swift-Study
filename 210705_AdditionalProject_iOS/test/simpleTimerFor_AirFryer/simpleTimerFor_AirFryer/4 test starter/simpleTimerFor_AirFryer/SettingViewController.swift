5a6e7cb07a2d05b05c6a778c2c310c571fe4cfca
jhy0409 <inooph@naver.com>
2021-07-20 17:52:11 +0900

    [o] setting - 배열에서

--- 210705_AdditionalProject_iOS/test/simpleTimerFor_AirFryer/simpleTimerFor_AirFryer/4 test starter/simpleTimerFor_AirFryer/SettingViewController.swift
+++ 210705_AdditionalProject_iOS/test/simpleTimerFor_AirFryer/simpleTimerFor_AirFryer/4 test starter/simpleTimerFor_AirFryer/SettingViewController.swift
@@ -13,13 +13,14 @@ class SettingTableViewController: UITableViewController {
     var addTmpFood: [Food] = []
     
     var foods: [Food] = []
+    let arr: [String] = ["서버에서 샘플받기", "버전", "3", "4", "5"]
     
     @IBOutlet weak var downSample: UISwitch!
     @IBOutlet weak var versionDescription: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        downSample.isOn = false
+//        downSample.isOn = false
         // Do any additional setup after loading the view.
     }
     
@@ -30,33 +31,43 @@ class SettingTableViewController: UITableViewController {
             downSample.isEnabled = false // 다운이 완료되면 초기화
             print("---> toggle On")
             
-//            self.db.setValue(<#T##value: Any?##Any?#>)
+            //            self.db.setValue(<#T##value: Any?##Any?#>)
         }
         // [] toggle버튼 OFF -> 기본 json file 다운로드
         else {
             print("---> toggle Off")
         }
         
-//        if downSample.isOn == true, FoodViewModel.countN_download < 1 {
-//            addTmpFood.append(<#T##newElement: Food##Food#>)
-//            foodViewModel.updateFood(addTmpFood)
-//            FoodViewModel.countN_download = 1
-//        }
+        //        if downSample.isOn == true, FoodViewModel.countN_download < 1 {
+        //            addTmpFood.append(<#T##newElement: Food##Food#>)
+        //            foodViewModel.updateFood(addTmpFood)
+        //            FoodViewModel.countN_download = 1
+        //        }
     }
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
+
+extension SettingTableViewController {
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        arr.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        else {
+            print("\n\n XXX dequeueReusableCell 실패 XXX")
+            let cell = UITableViewCell()
+            return cell
+        }
+        cell.textLabel?.text = arr[indexPath.row]
+        return cell
+    }
+}
 
 extension FoodViewModel {
     static var countN_download = 0
