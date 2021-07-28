6fa90abac6ded053c39be824a84e940f40d96fd7
jhy0409 <inooph@naver.com>
2021-07-29 08:37:42 +0900

    iOS앱 개인 프로젝트 생성 - 2번째

---
+++ 210705_AdditionalProject_iOS/dasiBom_for_dementiaPrevention/dasiBom_for_dementiaPrevention/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dasiBom_for_dementiaPrevention
+//
+//  Created by inooph on 2021/07/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
