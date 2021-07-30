f3d8ab115019a0f2add85ea1f3653e3b8853e36a
jhy0409 <inooph@naver.com>
2021-07-30 22:39:54 +0900

    ㅇ - 빌드만 성공..

--- 210705_AdditionalProject_iOS/dasiBom4/dasiBom4/ViewController.swift
+++ 210705_AdditionalProject_iOS/dasiBom4/dasiBom4/ViewController.swift
@@ -16,26 +16,52 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        
         guard let clientID = FirebaseApp.app()?.options.clientID else { return }
-
+        
         // Create Google Sign In configuration object.
         let config = GIDConfiguration(clientID: clientID)
-
+        
         // Start the sign in flow!
         GIDSignIn.sharedInstance.signIn(with: config, presenting: self) { [unowned self] user, error in
-
-          if let error = error { return }
-          guard let authentication = user?.authentication, let idToken = authentication.idToken else { return }
-
-          let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: authentication.accessToken)
-
-          // ...
+            if let error = error { return }
+            guard let authentication = user?.authentication, let idToken = authentication.idToken else { return }
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: authentication.accessToken)
+            
+            Auth.auth().signIn(with: credential) { authResult, error in
+                if let error = error {
+                    let authError = error as NSError
+                    //              if isMFAEnabled, authError.code == AuthErrorCode.secondFactorRequired.rawValue {
+                    // The user is a multi-factor user. Second factor challenge is required.
+                    let resolver = authError
+                        .userInfo[AuthErrorUserInfoMultiFactorResolverKey] as! MultiFactorResolver
+                    var displayNameString = ""
+                    for tmpFactorInfo in resolver.hints {
+                        displayNameString += tmpFactorInfo.displayName ?? ""
+                        displayNameString += " "
+                    }
+                } else {
+                    //                self.showMessagePrompt(error.localizedDescription)
+                    print(error!.localizedDescription)
+                    return
+                }
+                // ...
+                return
+            }
         }
         
-        
+        // User is signed in
+        // ...
     }
-
-
+    func signOut() {
+        let firebaseAuth = Auth.auth()
+        do {
+          try firebaseAuth.signOut()
+        } catch let signOutError as NSError {
+          print("Error signing out: %@", signOutError)
+        }
+    }
 }
+
+
+
 
