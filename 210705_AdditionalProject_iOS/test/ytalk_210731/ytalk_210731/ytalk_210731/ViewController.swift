89171c70389a07d99db89e25a57a4e8dded34adf
jhy0409 <inooph@naver.com>
2021-07-31 14:28:40 +0900

    와 정상작동하네

--- 210705_AdditionalProject_iOS/test/ytalk_210731/ytalk_210731/ytalk_210731/ViewController.swift
+++ 210705_AdditionalProject_iOS/test/ytalk_210731/ytalk_210731/ytalk_210731/ViewController.swift
@@ -7,12 +7,39 @@
 
 import UIKit
 import SnapKit
+import Firebase
 
 class ViewController: UIViewController {
     
     var box = UIImageView()
+    var remoteConfig: RemoteConfig!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        remoteConfig = RemoteConfig.remoteConfig()
+        let settings = RemoteConfigSettings()
+        settings.minimumFetchInterval = 0
+        remoteConfig.configSettings = settings
+        remoteConfig.setDefaults(fromPlist: "RemoteConfigDefaults")
+        
+        remoteConfig.fetch { (status, error) -> Void in
+          if status == .success {
+            print("Config fetched!")
+            self.remoteConfig.activate { changed, error in
+              // ...
+            }
+          } else {
+            print("Config not fetched")
+            print("Error: \(error?.localizedDescription ?? "No error available.")")
+          }
+          self.displayWelcome()
+        }
+        
+        
+        
+        
+        
+        
         // Do any additional setup after loading the view.
         self.view.addSubview(box)
         box.snp.makeConstraints { (make) in
@@ -20,9 +47,24 @@ class ViewController: UIViewController {
             make.width.height.equalTo(65)
         }
         box.image = #imageLiteral(resourceName: "chatAppIcon")
-        self.view.backgroundColor = UIColor(hex: "#000000")
+//        self.view.backgroundColor = UIColor(hex: "#000000")
     }
-
+    func displayWelcome() {
+        let color = remoteConfig["splash_background"].stringValue
+//        let caps = true
+        let caps = remoteConfig["splash_message_caps"].boolValue
+        let message = remoteConfig["splash_message_caps"].stringValue
+        
+        if(caps) {
+            let alert = UIAlertController(title: "공지사항", message: message, preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "확인", style: UIAlertAction.Style.default, handler: { (action) in
+                exit(0)
+            }))
+            self.present(alert, animated: true, completion: nil)
+        }
+        self.view.backgroundColor = UIColor(hex: color!)
+    }
+    
 
 }
 
