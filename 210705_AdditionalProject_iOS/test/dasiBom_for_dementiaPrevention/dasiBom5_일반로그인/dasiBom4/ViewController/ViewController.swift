d90d9c04f413cb576924435176c3b00f860093bf
jhy0409 <inooph@naver.com>
2021-08-03 01:16:01 +0900

    정상동작

--- 210705_AdditionalProject_iOS/test/dasiBom_for_dementiaPrevention/dasiBom5_일반로그인/dasiBom4/ViewController/ViewController.swift
+++ 210705_AdditionalProject_iOS/test/dasiBom_for_dementiaPrevention/dasiBom5_일반로그인/dasiBom4/ViewController/ViewController.swift
@@ -13,35 +13,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var emailTxtField: UITextField!
     @IBOutlet weak var pwTxtFIeld: UITextField!
     @IBOutlet weak var loginBtn: UIButton!
+    @IBOutlet weak var logOutBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        let user = Auth.auth().currentUser
-        if let user = user {
-            updateUI(user)
-        } else {
-            updateUI(user)
-        }
-//        self.view.layoutIfNeeded()
+        logOutBtn.isHidden = true
+        
+        if let user = Auth.auth().currentUser {
+            self.updateUI(user)
+        } else { self.updateUI(nil) }
     }
     
     
     @IBAction func loginBtnTouched(_ sender: Any) {
         let user = Auth.auth().currentUser
-        if user != nil {
-            signOut()
-            updateUI(user)
-        } else {
-            signIn()
-            updateUI(user)
-        }
+        self.signIn()
+    }
+    
+    @IBAction func logOutBtnTapped(_ sender: Any) {
+        signOut()
     }
     
     func signIn() {
         Auth.auth().signIn(withEmail: emailTxtField.text!, password: pwTxtFIeld.text!) { (user, error) in
             if user != nil  {
                 print("login success")
+                guard let tmpUser = Auth.auth().currentUser else { return }
+                self.updateUI(tmpUser)
             } else {
                 print("login fail")
             }
@@ -50,6 +48,8 @@ class ViewController: UIViewController {
     
     func signOut() {
         try? Auth.auth().signOut()
+        let user = Auth.auth().currentUser
+        updateUI(user)
     }
     
     func updateUI(_ user: User?) {
@@ -58,11 +58,14 @@ class ViewController: UIViewController {
             pwTxtFIeld.isHidden = true
             emailTxtField.text = ""
             pwTxtFIeld.text = ""
-            loginBtn.setTitle("로그아웃", for: .normal)
+            loginBtn.isHidden = true
+            logOutBtn.isHidden = false
+            
         } else { //미로그인 상태
             emailTxtField.isHidden = false
             pwTxtFIeld.isHidden = false
-            loginBtn.setTitle("로그인", for: .normal)
+            logOutBtn.isHidden = true
+            loginBtn.isHidden = false
         }
     }
     
