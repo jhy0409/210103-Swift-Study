3f8d261a13654037996024d020e298cfa99d4ad4
jhy0409 <inooph@naver.com>
2021-08-02 13:45:09 +0900

    할일 적어놓음

--- 210705_AdditionalProject_iOS/test/dasiBom_for_dementiaPrevention/dasiBom5_일반로그인/dasiBom4/ViewController/SurveyViewController.swift
+++ 210705_AdditionalProject_iOS/test/dasiBom_for_dementiaPrevention/dasiBom5_일반로그인/dasiBom4/ViewController/SurveyViewController.swift
@@ -10,7 +10,14 @@ import UIKit
 class SurveyViewController: UIViewController {
     let viewModel = QuestionViewModel.shared
     @IBOutlet weak var submitBtn: UIButton!
-    
+    /*
+     Todo
+     [] 제출버튼 클릭 시 파이어베이스 연동
+     [] 결과조회 탭 - 그전 검사결과 받아오기
+        - [] 예외처리 : 검사이력 없을 시 perform Segue(검사탭으로 이동)
+     
+     [] 게임하기 탭 - 게임이력 파이어베이스 연동
+     */
     override func viewDidLoad() {
         super.viewDidLoad()
         guard let navigationBar = navigationController?.navigationBar else { return }
@@ -25,7 +32,7 @@ class SurveyViewController: UIViewController {
          showAlert("검사결과", checkTrue())
     }
     
-    func checkTrue() -> (Bool, String) {
+    func checkTrue() -> (Bool, Int) {
         let tmpRange = 0...13
         let queArr = viewModel.getQueArr()
         var totalSum: Int = 0
@@ -34,11 +41,11 @@ class SurveyViewController: UIViewController {
             guard let tmpScore = queArr[i].score, let tmpChecked = queArr[i].checked
             else {
                 showAlert("알림창", "누락된 항목이 있습니다.")
-                return (false, "")
+                return (false, 0)
             }
             totalSum += queArr[i].score! // cell에서 체크시 점수를 주므로 강제추출 가능
         }
-        return (true, "\(totalSum)")
+        return (true, totalSum)
     }
     
     func showAlert(_ title: String, _ msg: String) {
@@ -48,10 +55,19 @@ class SurveyViewController: UIViewController {
         present(alert, animated: true, completion: nil)
     }
     
-    func showAlert(_ title: String, _ msg: (Bool, String)) {
+    func showAlert(_ title: String, _ msg: (Bool, Int)) {
         guard msg.0 == true else { return }
-        
-        let alert = UIAlertController(title: title, message: "검사결과는 \(msg.1)", preferredStyle: UIAlertController.Style.alert)
+
+        var tmpStr = "당신의 검사결과는 "
+        if msg.1 <= 5 {
+            tmpStr.append("\(msg.1)점 입니다.\n\n")
+            tmpStr.append("1~5점 운동과 외부 사회 활동을 유지하시고 \n치매예방 수칙 3·3·3을 잘 실천하셔서 치매를 에방하세요.")
+        } else {
+            tmpStr.append("\(msg.1)점 입니다.\n\n")
+            tmpStr.append("6~14점 가까운 보건소나 치매안심센터를 방문하셔서 더 정확한 치매검진을 받아보시기 바랍니다")
+        }
+
+        let alert = UIAlertController(title: title, message: tmpStr, preferredStyle: UIAlertController.Style.alert)
         let okAction = UIAlertAction(title: "확인", style: .default, handler: nil)
         alert.addAction(okAction)
         present(alert, animated: true, completion: nil)
