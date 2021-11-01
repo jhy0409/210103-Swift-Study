0888102c82483891924f72f7018044bcd4de16fd
jhy0409 <inooph@naver.com>
2021-11-01 10:27:50 +0900

    계산기 프로젝트

--- 210615_ otherLang_to_Swift/211031_MiniCalc/MiniCalc/ViewController.swift
+++ 210615_ otherLang_to_Swift/211031_MiniCalc/MiniCalc/ViewController.swift
@@ -32,6 +32,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         lblCalc.text = ""
+        lblContents.text = ""
     }
     
     // MARK: - [ㅇ] 0~9까지 버튼 탭 시 라벨 변경
@@ -97,16 +98,17 @@ class ViewController: UIViewController {
     
     // MARK: - [ㅇ] 연산
     @IBAction func submitAnswer(_ sender: UIButton) {
-        secondNum = Int(lblCalc.text ?? "0") ?? nil
+        secondNum = CLong(lblCalc.text ?? "0") ?? nil
         guard let first = firstNum, let second = secondNum
         else {
             let booleanForCalc = (firstNum == nil, secondNum == nil)
-            showAlert(bool: booleanForCalc, title: "에러발생")
+            showAlert(bool: booleanForCalc, title: "에러발생", checkLable: lblCalc.text)
             return
             
         }
         lblCalc.text = calc(num1: first, num2: second, oper: oper)
-        lblContents.text = "\(first) \(returnOperatorStr(oper: oper)) \(second) = \(lblCalc.text!)"
+        guard let lblcTxt = lblCalc.text else { return }
+        lblContents.text = "\(first) \(returnOperatorStr(oper: oper)) \(second) = \(lblcTxt)"
     }
     
     @IBAction func btnResetTapped(_ sender: UIButton) {
@@ -114,40 +116,47 @@ class ViewController: UIViewController {
     }
     
     func calc(num1: CLong, num2: CLong, oper: String) -> String? {
+        var result: (Int, Bool)
         switch oper {
         case "+":
-            let result = num1 + num2
-            if result < CLong.min || result > CLong.max {
-                let messageString = result < CLong.min ? "min 범위 초과 : \(CLong.min)" : "max 범위 초과 : \(CLong.max)"
-                showAlert(title: "에러 발생", message: "\(messageString)")
-                return nil
-            }
-            return "\(num1 + num2)"
-            
-        case "-":
-            let result = num1 - num2
-            if result < CLong.min || result > CLong.max {
-                let messageString = result < CLong.min ? "min 범위 초과 : \(CLong.min)" : "max 범위 초과 : \(CLong.max)"
-                showAlert(title: "에러 발생", message: "\(messageString)")
-                return nil
-            }
-            return "\(num1 - num2)"
-            
-        case "*":
-            let tempBool = (num1 * num2 < CLong.min, num1 * num2 > CLong.max)
-            if tempBool.0 == true || tempBool.1 == true {
-                let messageString = tempBool.0 == true ? "min 범위 초과 : \(CLong.min)" : "max 범위 초과 : \(CLong.max)"
-                showAlert(title: "에러 발생", message: "\(messageString)")
+            result = num1.addingReportingOverflow(num2)
+            if result.1 == true {
+                showAlert(title: "오버플로우 발생", message: "범위 초과로 값을 초기화합니다.")
                 return nil
             } else {
-                return "\(num1 * num2)"
+                return "\(result.0)"
+            }
+            
+        case "-":
+            let absNum2 = abs(num2)
+            result = num1.subtractingReportingOverflow(absNum2)
+            if result.1 == true {
+                showAlert(title: "오버플로우 발생", message: "범위 초과로 값을 초기화합니다.")
+                return nil
+            } else {
+                return "\(result.0)"
+            }
+            
+        case "*":
+            result = num1.multipliedReportingOverflow(by: num2)
+            if result.1 == true {
+                showAlert(title: "오버플로우 발생", message: "범위 초과로 값을 초기화합니다.")
+                return nil
+            } else {
+                return "\(result.0)"
             }
             
         default:
-            if(num2 <= 0) {
-                showAlert(bool: num2<=0, title: "에러 발생")
-                return nil }
-            return "\(num1 / num2)"
+            result = num1.dividedReportingOverflow(by: num2)
+            if result.1 == true {
+                if(num2 <= 0) {
+                    showAlert(bool: num2<=0, title: "에러 발생")
+                    return nil }
+                showAlert(title: "오버플로우 발생", message: "범위 초과로 값을 초기화합니다.")
+                return nil
+            } else {
+                return "\(result.0)"
+            }
         }
     }
     
@@ -160,14 +169,19 @@ class ViewController: UIViewController {
     }
     
     // MARK: - [ㅇ] 경고창
-    private func showAlert(bool: (num1: Bool, num2: Bool), title: String) {
+    private func showAlert(bool: (num1: Bool, num2: Bool), title: String, checkLable: String?) {
         var message = ""
+        
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
         
         if bool.num1 == true {
             message.append("- 첫번째 숫자가 유효하지 않은 값입니다.\n- 첫번째 숫자는 연산자를 누를 때 저장됩니다.\n- 첫번째 숫자 > 연산자 > 두번째 숫자 > = 순서로 입력하십시오\n\n")
         }
         if bool.num2 == true {
-            message.append("- 두번째 숫자가 유효하지 않은 값입니다.\n")
+            let minC = "\(CLong.min)"
+            let maxC = "\(CLong.min)"
+            message.append("- 두번째 숫자가 유효하지 않은 값입니다. \n\ncLong 최대 및 최소값 : \(numberFormatter.string(from: NSNumber(value: CLong.min))!) ~ \(numberFormatter.string(from: NSNumber(value: CLong.max))!)\n 음수 최대자리 수 \(minC.count) ~ 양수 최대자리 수 \(maxC.count) - 현재 입력값 자리 수 \(checkLable!.count)\n\n유효하지 않은 입력으로 값을 초기화합니다.")
         }
         
         print(" 1 \(bool.num1) / 2 \(bool.num2)")
