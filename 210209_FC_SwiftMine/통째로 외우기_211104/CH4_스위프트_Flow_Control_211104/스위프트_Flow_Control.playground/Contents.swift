fb6cedfa0d8123932f0d74ae3e64ce880c06cce1
jhy0409 <inooph@naver.com>
2021-11-04 20:37:26 +0900

    CH4_스위프트_Flow_Control_211104

--- 210209_FC_SwiftMine/통째로 외우기_211104/CH4_스위프트_Flow_Control_211104/스위프트_Flow_Control.playground/Contents.swift
+++ 210209_FC_SwiftMine/통째로 외우기_211104/CH4_스위프트_Flow_Control_211104/스위프트_Flow_Control.playground/Contents.swift
@@ -114,3 +114,63 @@ for i in closedRange where i > 1 && i < 10{
 /*: ## Switch */
 
 printChapterTitle(str: "Switch", isDone: 2)
+
+var num = 10
+
+switch num {
+case 0:
+    print("---> 0입니다.")
+
+case 0..<10:
+    print("---> 0 ~ 10 사이입니다.")
+
+case 10:
+    print("---> 10입니다.")
+    
+default:
+    print("---> 그 외 나머지")
+}
+
+
+let pet = "bird"
+
+switch pet {
+case "dog", "cat", "bird":
+    print("집 동물")
+default:
+    print("그 외 나머지 동물")
+}
+
+num = 50
+
+switch num {
+case _ where num % 2 == 0:
+    print("짝수")
+default:
+    print("홀수")
+}
+
+/*
+ - 1 : x < 0, y > 0
+ - 2 : x > 0, y > 0
+ - 3 : x > 0, y < 0
+ - 4 : x < 0, y < 0
+ */
+let coordinate = (x: 20, y: -30)
+
+switch coordinate {
+case (0, 0):
+    print("원점")
+    
+case (let x, 0):
+    print("x축, x : \(x)")
+    
+case (0, let y):
+    print("y축, y : \(y)")
+    
+case (let x, let y) where x == y:
+    print("x, y값이 같습니다. = \(x), \(y)")
+    
+case (let x, let y):
+    print("좌표 어딘가. x = \(x), y = \(y)")
+}
