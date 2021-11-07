1f285bc69f8ab7cf097fd9f5bc9f1060ef94d7bb
jhy0409 <inooph@naver.com>
2021-11-07 14:29:58 +0900

    chap 7 - 3 start

--- 210209_FC_SwiftMine/통째로 외우기_211104/CH7_스위프트_Structure/3_properties.playground/Contents.swift
+++ 210209_FC_SwiftMine/통째로 외우기_211104/CH7_스위프트_Structure/3_properties.playground/Contents.swift
@@ -1,3 +1,38 @@
 import UIKit
 
-var greeting = "Hello, playground"
+/*: ## N번째*/
+// MARK: - [] Person struct
+// MARK: - [] 1. 속성
+    // var firstName - willSet, didSet
+    // var lastName
+    
+    // lazy var isPopular: Bool - Jay Park
+    // var fullName - get, set / func fullName
+
+    // static let isAlien - f
+
+// MARK: - [] 2. var person - Jason, Lee
+    // 1. Jim Kim ->
+    // Jay Park >
+
+    // Person.isAlien
+    // person.isPopular
+
+
+/*: ## 1번째*/
+// MARK: - [] Person struct
+// MARK: - [] 1. 속성
+    // var firstName - willSet, didSet
+    // var lastName
+    
+    // lazy var isPopular: Bool - Jay Park
+    // var fullName - get, set / func fullName
+
+    // static let isAlien - f
+
+// MARK: - [] 2. var person - Jason, Lee
+    // 1. Jim Kim ->
+    // Jay Park >
+
+    // Person.isAlien
+    // person.isPopular
