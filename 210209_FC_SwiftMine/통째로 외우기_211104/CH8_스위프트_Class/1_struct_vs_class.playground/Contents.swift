aedb5687e9b6ef4c2b63f06a69b4310cb2bc11b1
jhy0409 <inooph@naver.com>
2021-11-07 21:51:36 +0900

    chap 8 start

---
+++ 210209_FC_SwiftMine/통째로 외우기_211104/CH8_스위프트_Class/1_struct_vs_class.playground/Contents.swift
@@ -0,0 +1,92 @@
+import UIKit
+
+/*: ## N번째*/
+// MARK: - [] 1. PersonStruct
+    // var firstName, lastName, 생성자
+    // var fullName(계산속성)
+    // func uppercaseName() : first, last
+
+// MARK: - [] 2. PersonClass
+    // var firstName, lastName, 생성자
+    // var fullName(계산속성)
+    // func uppercaseName() : first, last
+
+// MARK: - [] personStruct1 - Jason Lee, per...2 = per..1할당
+// MARK: - [] personClass1 - Jason Lee, perC...2 = per..1할당
+
+/*
+ personStruct2.firstName = "Jay"
+ personStruct1.firstName
+ personStruct2.firstName
+ 
+ personClass2.firstName = "Jay"
+ personClass1.firstName
+ personClass2.firstName
+ 
+ personClass1 = personClass2
+ personClass1.firstName
+ personClass2.firstName
+ */
+
+// MARK: - [] struct Point - let x, let y: Int
+    /*
+     point1 = 3, 5
+     point2 = 3, 5
+     */
+
+// MARK: - [] struct Mac
+    // var owner
+
+/*
+ myMac = "Jason"
+ yourMac = myMac
+ yourMac.owner = "Jay"
+ 
+ myMac.owner
+ yourMac.owner
+ */
+/*: ## 1번째*/
+// MARK: - [] 1. PersonStruct
+    // var firstName, lastName, 생성자
+    // var fullName(계산속성)
+    // func uppercaseName() : first, last
+
+// MARK: - [] 2. PersonClass
+    // var firstName, lastName, 생성자
+    // var fullName(계산속성)
+    // func uppercaseName() : first, last
+
+// MARK: - [] personStruct1 - Jason Lee, per...2 = per..1할당
+// MARK: - [] personClass1 - Jason Lee, perC...2 = per..1할당
+
+/*
+ personStruct2.firstName = "Jay"
+ personStruct1.firstName
+ personStruct2.firstName
+ 
+ personClass2.firstName = "Jay"
+ personClass1.firstName
+ personClass2.firstName
+ 
+ personClass1 = personClass2
+ personClass1.firstName
+ personClass2.firstName
+ */
+
+// MARK: - [] struct Point - let x, let y: Int
+    /*
+     point1 = 3, 5
+     point2 = 3, 5
+     */
+
+// MARK: - [] struct Mac
+    // var owner
+
+/*
+ myMac = "Jason"
+ yourMac = myMac
+ yourMac.owner = "Jay"
+ 
+ myMac.owner
+ yourMac.owner
+ */
