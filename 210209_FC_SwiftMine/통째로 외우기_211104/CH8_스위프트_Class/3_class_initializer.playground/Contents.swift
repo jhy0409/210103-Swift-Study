a8c0e32e2f911ecdbdd4524e67e6621e8abaebb5
jhy0409 <inooph@naver.com>
2021-11-14 20:13:53 +0900

    chap 8 - 3_class_initializer start

--- 210209_FC_SwiftMine/통째로 외우기_211104/CH8_스위프트_Class/3_class_initializer.playground/Contents.swift
+++ 210209_FC_SwiftMine/통째로 외우기_211104/CH8_스위프트_Class/3_class_initializer.playground/Contents.swift
@@ -1,3 +1,31 @@
 import UIKit
 
-var greeting = "Hello, playground"
+/*: ## N번째*/
+// MARK: - [] struct Grade var letter(Ch), points, credits(Double)
+
+// MARK: - [] class Person var firstName, lastName, init, func printMyName()
+
+// MARK: - [] class Student : Person, var grades: [Grade] = []
+    // init(firstName, lastName)
+    // convenience(student: Student)
+
+// MARK: - [] class StudentAthlete: Student 학생인데 운동선수
+    // var minimunTrainingTime: Int = 2
+    // var trainedTime = 0
+    // var sports: [String]
+
+    // init
+    // convenience init
+    // func train() += 1
+
+// MARK: - [] class FootballPlayer: StudentAth... 운동선수인데 축구선수
+
+    // var footballTeam = "FC Swift"
+    // train() += 2
+
+// MARK: - [] try..
+    // student1 = Jason Lee
+    // student1_1 = Student(student: student1)
+
+    // student2 = StudentAthlete Jay, Lee, Football
+    // student3 = Mike
