1ab4c9fe2cff2a5b1a54007276f0c8eeade3f70f
jhy0409 <inooph@naver.com>
2021-11-30 22:03:14 +0900

    done - MVVM 3rd

--- 210209_FC_SwiftMine/통째로 외우기_211104/CH10_MVVM 디자인 패턴/3/BountyList/BountyList/DetailViewController.swift
+++ 210209_FC_SwiftMine/통째로 외우기_211104/CH10_MVVM 디자인 패턴/3/BountyList/BountyList/DetailViewController.swift
@@ -29,7 +29,9 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var imgView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var bountyLabel: UILabel!
-
+    
+    let viewModel = DetailViewModel()
+    
     var name: String?
     var bounty: Int?
     
@@ -39,15 +41,22 @@ class DetailViewController: UIViewController {
     }
     
     func updateUI() {
-        if let name = self.name, let bounty = self.bounty {
-            let img = UIImage(named: "\(name).jpg")
-            imgView.image = img
-            nameLabel.text = name
-            bountyLabel.text = "\(bounty)"
+        if let bountyInfo = viewModel.bountyInfo {
+            imgView.image = bountyInfo.image
+            nameLabel.text = bountyInfo.name
+            bountyLabel.text = "\(bountyInfo.bounty)"
         }
     }
     
     @IBAction func close(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+}
+
+class DetailViewModel {
+    var bountyInfo: BountyInfo?
+    
+    func update(model: BountyInfo?) {
+        bountyInfo = model
     }
 }
